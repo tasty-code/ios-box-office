e29e9ea2f5d0533d453517b5deddd7d106962a5e
Jason <ggj00125@gmail.com>
2023-04-24 16:52:14 +0900

    [Feat]: UnitTest Target 추가

---
+++ MockMovieJSONDecodingTest/MockMovieJSONDecodingTest.swift
@@ -0,0 +1,23 @@
+//
+//  MockMovieJSONDecodingTest.swift
+//  MockMovieJSONDecodingTest
+//
+//  Created by Jason on 2023/04/24.
+//
+
+import XCTest
+
+final class MockMovieJSONDecodingTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+}
