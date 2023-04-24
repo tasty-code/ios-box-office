e58fe42abe49b602d3653c60f15786b9ac95c54d
Mason Kim <qwerty3345@naver.com>
2023-04-24 17:19:25 +0900

    [Chore] 주석 creator 수정

--- BoxOffice/SceneDelegate.swift
+++ BoxOffice/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  BoxOffice
 //
-//  Created by kjs on 13/01/23.
+//  Created by M&A on 13/01/23.
 //
 
 import UIKit
--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  BoxOffice
 //
-//  Created by kjs on 13/01/23.
+//  Created by M&A on 13/01/23.
 //
 
 import UIKit
