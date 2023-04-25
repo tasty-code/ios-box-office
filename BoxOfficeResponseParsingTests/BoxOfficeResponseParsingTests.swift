3fc1b3d8fd5da781fde85bfa1e930cbeeaf0986b
Mason Kim <qwerty3345@naver.com>
2023-04-25 10:16:12 +0900

    [Test] Response 파싱 테스트 타겟 추가

---
+++ BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
@@ -0,0 +1,20 @@
+//
+//  BoxOfficeResponseParsingTests.swift
+//  BoxOfficeResponseParsingTests
+//
+//  Created by Mason Kim on 2023/04/25.
+//
+
+import XCTest
+
+final class BoxOfficeResponseParsingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
