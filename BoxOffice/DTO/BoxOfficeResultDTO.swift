1ab01eb433cb57c45dd72840ff7eb00f7870721b
Sunnnysong <tjsghktjswls@naver.com>
2023-04-25 11:38:51 +0900

    feat: Network DTO 타입 생성
    - DTO 폴더 생성
    - DTO 타입 생성

---
+++ BoxOffice/DTO/BoxOfficeResultDTO.swift
@@ -0,0 +1,13 @@
+//
+//  BoxOfficeResultDTO.swift
+//  BoxOffice
+//
+//  Created by Sunny on 2023/04/25.
+//
+
+import Foundation
+
+struct BoxOfficeResultDTO: Decodable {
+    let boxofficeType, showRange: String
+    let dailyBoxOfficeList: [DailyBoxOfficeListDTO]
+}
---
+++ BoxOffice/DTO/RankOldAndNewDTO.swift
@@ -0,0 +1,13 @@
+//
+//  RankOldAndNewDTO.swift
+//  BoxOffice
+//
+//  Created by Sunny on 2023/04/25.
+//
+
+import Foundation
+
+enum RankOldAndNewDTO: String, Decodable {
+    case new = "NEW"
+    case old = "OLD"
+}
