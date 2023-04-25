208fc4825408edf01601e328c7c1047aa0197db5
devxsby <devxsby@gmail.com>
2023-04-25 11:51:02 +0900

    [Test] BoxOfficeResponseParsingTests 추가

--- BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
+++ BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
@@ -9,17 +9,23 @@ import XCTest
 
 final class BoxOfficeResponseParsingTests: XCTestCase {
     
-    var sut: Data!
-
+    var sut: DailyBoxOfficeResponse!
+    
     override func setUpWithError() throws {
-        let fileLocation = Bundle.main.url(forResource: "box_office_sample", withExtension: "json")!
+        super.setUp()
         
-        XCTAssertNoThrow(
-            sut = try Data(contentsOf: fileLocation)
-        )
+        let fileLocation = Bundle.main.url(forResource: "box_office_sample", withExtension: "json")!
+        let data = try Data(contentsOf: fileLocation)
+        
+        sut = try JSONDecoder().decode(DailyBoxOfficeResponse.self, from: data)
     }
 
     override func tearDownWithError() throws {
         sut = nil
+        super.tearDown()
+    }
+    
+    func test_데일리박스오피스배열의_첫번째영화코드가_일치하는지() {
+        XCTAssertEqual(sut.boxOfficeResult.dailyBoxOfficeList[0].movieCode, "20199882")
     }
 }
