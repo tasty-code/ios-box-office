9e0e1cbef0cbc904f07dc96bc1de209f0034cf98
longlivedrgn <kimye9808@naver.com>
2023-04-25 16:24:20 +0900

    feat: BoxOfficeAPIEndpoints 열거형 속 상수 및 QueryItems를 만드는 함수 정의

--- BoxOffice/BoxOfficeAPIEndpoints.swift
+++ BoxOffice/BoxOfficeAPIEndpoints.swift
@@ -18,11 +18,33 @@ extension BoxOfficeAPIEndpoints {
 
     var endPoint: EndPoint {
         switch self {
-        case .boxOffice(targetDate: let date):
-            return EndPoint(baseURL: "http://www.kobis.or.kr", path: "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json")
-        case .movieDetail(movieCode: let movieCode):
-            return EndPoint(baseURL: "http://www.kobis.or.kr", path: "/kobisopenapi/webservice/rest/movie/searchMovieInfo.json")
+        case .boxOffice:
+            return EndPoint(baseURL: "http://www.kobis.or.kr", path: "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json", queryItems: makeQueryItems())
+        case .movieDetail:
+            return EndPoint(baseURL: "http://www.kobis.or.kr", path: "/kobisopenapi/webservice/rest/movie/searchMovieInfo.json", queryItems: makeQueryItems())
         }
 
     }
+    
+    enum QueryConstant {
+        static let apiKeyQueryName = "key"
+        static let apiKeyQueryValue = "6c4b02fc76306e47a3ada0534d4cc519"
+        static let movieCodeQueryName = "movieCd"
+        static let targetDateQueryName = "targetDt"
+    }
+    
+    func makeQueryItems() -> [URLQueryItem] {
+        let apiKeyQueryItem = URLQueryItem(name: QueryConstant.apiKeyQueryName, value: QueryConstant.apiKeyQueryValue)
+        
+        switch self {
+        case .boxOffice(targetDate: let date):
+            let dateQueryItem = URLQueryItem(name: QueryConstant.targetDateQueryName, value: "\(date)")
+            return [apiKeyQueryItem, dateQueryItem]
+        case .movieDetail(movieCode: let code):
+            let movieCodeQueryItem = URLQueryItem(name: QueryConstant.movieCodeQueryName, value: "\(code)")
+            return [apiKeyQueryItem, movieCodeQueryItem]
+        }
+    }
+    
 }
+
