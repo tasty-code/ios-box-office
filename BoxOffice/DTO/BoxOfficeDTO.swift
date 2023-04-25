f5cf483c45df459b5bbb653088b6e74a10d8fe68
Sunnnysong <tjsghktjswls@naver.com>
2023-04-25 14:53:45 +0900

    feat: BoxOfficeDTO 생성

---
+++ BoxOffice/DTO/BoxOfficeDTO.swift
@@ -0,0 +1,12 @@
+//
+//  BoxOfficeDTO.swift
+//  BoxOffice
+//
+//  Created by Sunny on 2023/04/25.
+//
+
+import Foundation
+
+struct BoxOfficeDTO: Decodable {
+    let boxOfficeResult: BoxOfficeResultDTO
+}
