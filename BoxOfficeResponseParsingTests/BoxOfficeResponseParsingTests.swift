d737d5575456304757964cba2832c2242bde8d60
devxsby <devxsby@gmail.com>
2023-04-25 14:30:05 +0900

    [Chore] super.setUp, super.teardown 제거

--- BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
+++ BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
@@ -12,17 +12,14 @@ final class BoxOfficeResponseParsingTests: XCTestCase {
     var sut: Data!
     
     override func setUpWithError() throws {
-        super.setUp()
-        
         let fileLocation = Bundle.main.url(forResource: "box_office_sample", withExtension: "json")!
         XCTAssertNoThrow(
-             sut = try Data(contentsOf: fileLocation)
+            sut = try Data(contentsOf: fileLocation)
         )
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
-        super.tearDown()
     }
     
     func test_JSON파일의_Data를_디코딩했을때의_결과값이_비어있지_않다() throws {
