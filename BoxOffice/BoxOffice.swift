4e3f981044ec5e11e5373867d694ee1a8ce7313c
sloDevz <onec555@gmail.com>
2023-04-26 15:30:09 +0900

    refactor: 리뷰 반영

--- BoxOffice/BoxOffice.swift
+++ BoxOffice/BoxOffice.swift
@@ -6,14 +6,12 @@
 //
 import Foundation
 
-// MARK: - BoxOffice
 struct BoxOffice: Decodable {
 
     let boxOfficeResult: BoxOfficeResult
 
 }
 
-// MARK: - BoxOfficeResult
 struct BoxOfficeResult: Decodable {
 
     let boxOfficeType: String
@@ -30,7 +28,6 @@ struct BoxOfficeResult: Decodable {
 
 }
 
-// MARK: - DailyBoxOfficeList
 struct DailyBoxOfficeList: Decodable {
 
     let orderNumber: String
--- BoxOffice/JSONDeserializer.swift
+++ BoxOffice/JSONDeserializer.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class JSONDeserializer {
+final class JSONDeserializer {
 
     // MARK: - Properties
 
