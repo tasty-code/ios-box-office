60bbe970126390c6d309ef05f80163be41632b6e
Jason <ggj00125@gmail.com>
2023-04-26 17:57:43 +0900

    [Feat]: Basic enum 타입 구현

---
+++ BoxOffice/Data/Network/Query/Basic.swift
@@ -0,0 +1,13 @@
+//
+//  Basic.swift
+//  BoxOffice
+//
+//  Created by Jason on 2023/04/26.
+//
+
+import Foundation
+
+enum Basic {
+    static let baseURL: String = "http://www.kobis.or.kr/kobisopenapi/webservice/rest"
+    static let format: String = ".json"
+}
