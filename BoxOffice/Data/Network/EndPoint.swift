e7462d17ae7f267a91f66f8e0df4e468b244fbf1
Jason <ggj00125@gmail.com>
2023-04-26 17:58:45 +0900

    [Feat]: EndPoint configureURL 메서드 초기 구현

--- BoxOffice/Data/Network/EndPoint.swift
+++ BoxOffice/Data/Network/EndPoint.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+enum HTTPMethodType: String {
+    case get = "GET"
+}
+
 struct EndPoint {
     
+    static func configureURL() {
+        
+        guard var components = URLComponents(string: "\(Basic.baseURL)\(Show.boxOffice)\(Show.searchDailyList)\(Basic.format)") else {
+            return
+        }
+        
+        let key = URLQueryItem(name: "key", value: "f5eef3421c602c6cb7ea224104795888")
+        let targetDate = URLQueryItem(name: "targetDt", value: "20120101")
+        components.queryItems = [key, targetDate]
+        
+        guard let url = components.url else { return }
+    }
 }
