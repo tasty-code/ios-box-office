1a038ac5a1dcb90c1cd44d905564d12a19709255
Lust3r <s1skyblue1s@gmail.com>
2023-04-26 17:16:05 +0900

    [Feat]: Query 그룹 안에 Movie, Show enum type 구현

---
+++ BoxOffice/Data/Network/Query/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  BoxOffice
+//
+//  Created by 이상윤 on 2023/04/26.
+//
+
+import Foundation
+
+enum Movie {
+    static let category: String = "movie"
+    static let searchMovieInfo: String = "searchMovieInfo"
+}
---
+++ BoxOffice/Data/Network/Query/Show.swift
@@ -0,0 +1,13 @@
+//
+//  Show.swift
+//  BoxOffice
+//
+//  Created by 이상윤 on 2023/04/26.
+//
+
+import Foundation
+
+enum Show {
+    static let boxOffice: String = "boxoffice"
+    static let searchDailyList: String = "searchDailyBoxOfficeList"
+}
