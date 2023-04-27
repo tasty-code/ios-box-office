1a06b727ccdc154a413c44c3450f851212759f9a
Lust3r <s1skyblue1s@gmail.com>
2023-04-27 17:37:25 +0900

    [Feat]: DTO case를 담은 MovieDTO enum 타입 구현

---
+++ BoxOffice/Data/MovieDTO/MovieDTO.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDTO.swift
+//  BoxOffice
+//
+//  Created by 이상윤 on 2023/04/27.
+//
+
+import Foundation
+
+enum MovieDTO: CaseIterable {
+    case boxOffice
+    case movieDetailInformation
+}
