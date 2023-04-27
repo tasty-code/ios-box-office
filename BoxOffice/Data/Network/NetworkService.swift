e6098a33501e5207f338e880b80b5c0c949456af
Lust3r <s1skyblue1s@gmail.com>
2023-04-27 21:24:20 +0900

    [Feat]: NetworkService 내 URLSession 기능 구현
    - async/await을 사용한 비동기 코드 처리
    - do~catch를 사용하여 NetworkError 핸들링

--- BoxOffice/Data/Network/NetworkService.swift
+++ BoxOffice/Data/Network/NetworkService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class NetworkService {
-    
+
     private let session: URLSession
     private let endPoint: EndPoint
     
@@ -16,5 +16,24 @@ class NetworkService {
         self.session = session
         self .endPoint = endPoint
     }
-    
+
+    func request(from url: URL) async throws {
+        var result: Data?
+        var urlResponse: URLResponse?
+
+        do {
+            let (data, response) = try await session.data(for: endPoint.urlRequest(from: url))
+            result = data
+            urlResponse = response as? HTTPURLResponse
+
+
+        } catch {
+            throw NetworkError.urlRequest
+        }
+    }
+
+    //TODO: - 디코딩하는 메서드 구현
+
+    //TODO: - URLResponse 에러 처리 메서드 구현
+
 }
