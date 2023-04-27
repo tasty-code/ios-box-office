60123169abf0fa33881af7f3ffcf8bba1b3f2dd7
Jason <ggj00125@gmail.com>
2023-04-27 14:58:50 +0900

    [Feat]: APIMagicLiteral 타입 구현

---
+++ BoxOffice/Data/Network/Query/APIMagicLiteral.swift
@@ -0,0 +1,14 @@
+//
+//  APIMagicLiteral.swift
+//  BoxOffice
+//
+//  Created by Jason on 2023/04/27.
+//
+
+import Foundation
+
+enum APIMagicLiteral {
+    static let Key = "key"
+    static let KeyValue = "f5eef3421c602c6cb7ea224104795888"
+    static let targetQuery = "targetDt"
+}
