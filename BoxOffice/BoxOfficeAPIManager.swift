e0f7a9eae747fc9d096885d54db3025282a78786
longlivedrgn <kimye9808@naver.com>
2023-04-27 14:29:51 +0900

    feat: BoxOfficeAPIManager 타입 생성

---
+++ BoxOffice/BoxOfficeAPIManager.swift
@@ -0,0 +1,36 @@
+//
+//  BoxOfficeAPIManager.swift
+//  BoxOffice
+//
+//  Created by 김용재 on 2023/04/27.
+//
+
+import Foundation
+
+struct BoxOfficeAPIManager {
+    
+    let deserializer = JSONDeserializer()
+    let networkDispatcher = NetworkDispatcher()
+    
+    func fetchData(to type: Decodable.Type, endPoint: BoxOfficeAPIEndpoints, completionHandler: @escaping (Decodable) -> Void) {
+        
+        let urlRequest = endPoint.urlRequest
+        
+        networkDispatcher.performRequest(urlRequest) { networkResult in
+            
+            switch networkResult {
+                
+            case .success(let data):
+                do {
+                    let decodedData = try deserializer.deserialize(type: type, data: data)
+                    completionHandler(decodedData)
+                }
+                catch {
+                    print(NetworkError.failedDecoding.errorDescription)
+                }
+            case .failure(let error):
+                print(error.errorDescription)
+            }
+        }
+    }
+}
