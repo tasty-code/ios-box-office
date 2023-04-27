efc7312008e54476327f9abed161254e2e96f095
Mason Kim <qwerty3345@naver.com>
2023-04-27 14:53:29 +0900

    [Feat] NetworkRouter 구현

---
+++ BoxOffice/Service/NetworkRouter.swift
@@ -0,0 +1,68 @@
+//
+//  NetworkRouter.swift
+//  BoxOffice
+//
+//  Created by Mason Kim on 2023/04/27.
+//
+
+import Foundation
+
+typealias NetworkRouterCompletion = (Result<Data, Error>) -> Void
+
+protocol NetworkRouterProtocol: AnyObject {
+    associatedtype EndPoint: EndPointType
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
+    func cancel()
+}
+
+final class NetworkRouter<EndPoint: EndPointType>: NetworkRouterProtocol {
+    
+    // MARK: - Properties
+    
+    private var task: URLSessionTask?
+    private let session: URLSession
+    
+    // MARK: - Initialization
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    // MARK: - Public Methods
+    
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion) {
+        let request = buildRequest(from: route)
+        
+        task = session.dataTask(with: request, completionHandler: { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                // TODO: Network 에러 정의, statusCode Error 던지기
+                completion(.failure(error!))
+                return
+            }
+            
+            guard let data else {
+                // TODO: Network 에러 정의, Data Error 던지기
+                completion(.failure(error!))
+                return
+            }
+            
+            completion(.success(data))
+        })
+        self.task?.resume()
+    }
+    
+    func cancel() {
+        self.task?.cancel()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func buildRequest(from route: EndPoint) -> URLRequest {
+        var request = URLRequest(url: route.baseURL.appendingPathComponent(route.path),
+                                 timeoutInterval: 10.0)
+        
+        request.httpMethod = route.httpMethod.rawValue
+        return request
+    }
+}
