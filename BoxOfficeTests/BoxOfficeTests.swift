66b191cc0fd445036d42f66ee4fb1c0ae558e1af
doshkor <dosh.kor@gmail.com>
2023-04-27 14:38:41 +0900

    Refactor: Unit Test 메서드 내부 로직 변경 #4 - 파싱 결과값을 검증하는 로직에서 파싱 성공여부를 검증하는 로직으로 변경

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -18,17 +18,14 @@ final class BoxOfficeTests: XCTestCase {
         super.tearDown()
     }
     
-    func test_JSON데이터와_DailyBoxOffice로_디코딩한객체의_showRange와name이_일치한다() {
+    func test_dailyBoxOfficeData는_DailyBoxOffice로_디코딩_가능하다() {
         // given
-        let expectedShowRange = "20220105~20220105"
-        let expectedName = "경관의 피"
+        let jsonData = dailyBoxOfficeData
         
         // when
-        let parsedData = try? JSONDecoder().decode(DailyBoxOffice.self, from: dailyBoxOfficeData)
-        guard let showRange = parsedData?.boxOfficeResult.showRange,
-              let name = parsedData?.boxOfficeResult.dailyBoxOfficeList.first?.name else { return }
+        let parsedData = try? JSONDecoder().decode(DailyBoxOffice.self, from: jsonData)
         
         // then
-        XCTAssertEqual([showRange, name], [expectedShowRange, expectedName])
+        XCTAssertNotNil(parsedData)
     }
 }
