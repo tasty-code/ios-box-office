8459e7e20bb1b951a3ce5b29b76e400af2c6cc66
devxsby <devxsby@gmail.com>
2023-04-27 14:16:53 +0900

    [Feat] Service 파일 EndPointType 추가

---
+++ BoxOffice/Service/EndPointType.swift
@@ -0,0 +1,16 @@
+//
+//  EndPointType.swift
+//  BoxOffice
+//
+//  Created by devxsby on 2023/04/27.
+//
+
+import Foundation
+
+protocol EndPointType {
+    var baseURL: URL { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var task: HTTPTask { get }
+    var headers: HTTPHeaders? { get }
+}
---
+++ BoxOffice/Service/HTTPTask.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPTask.swift
+//  BoxOffice
+//
+//  Created by devxsby on 2023/04/27.
+//
+
+import Foundation
+
+typealias HTTPHeaders = [String: String]
+
+enum HTTPTask {
+    case request
+}
