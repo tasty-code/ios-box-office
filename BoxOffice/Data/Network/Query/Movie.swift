d0756efa4c91f3ffe65ce6553ac0db0cb67cf5e9
Jason <ggj00125@gmail.com>
2023-04-27 20:53:24 +0900

    [Feat]: 빠진 "/" 문자 추가

--- BoxOffice/Data/Network/Query/Movie.swift
+++ BoxOffice/Data/Network/Query/Movie.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum Movie {
-    static let category: String = "movie"
-    static let searchMovieInfo: String = "searchMovieInfo"
+    static let category: String = "/movie"
+    static let searchMovieInfo: String = "/searchMovieInfo"
 }
