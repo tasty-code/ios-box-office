9461dd9951242c648ba1150439afe80f689d8543
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-04-27 13:54:41 +0900

    refactor: test 메서드 분리 및 json 파일 참조 수정
    - test 케이스에 필요한 json 파일의 경로를 찾아오는 기능을 `findFile(name: , withExtension: ) -> URL?` 메서드로 분리
    - 파일의 경로를 찾아오는 메서드를 setUpWithError()` 메서드 내부에서 호출
    - 깨진 json 파일의 참조 수정`

--- BoxOfficeTests/JSONDeserializerTest.swift
+++ BoxOfficeTests/JSONDeserializerTest.swift
@@ -11,32 +11,32 @@ import XCTest
 final class JSONDeserializerTest: XCTestCase {
     
     var decoder: JSONDeserializable!
+    var fileURL: URL!
     
     override func setUpWithError() throws {
-        
+
+        let fileName = "box_office_sample"
+        let fileExtension = "json"
+
         try super.setUpWithError()
         decoder = JSONDeserializer()
+        fileURL = findFile(name: fileName, withExtension: fileExtension)
     }
 
     override func tearDownWithError() throws {
-        
-        decoder = nil
+
         try super.tearDownWithError()
+        decoder = nil
     }
 
     func test_DTO타입으로_Parsing되는지() throws {
-        
+
         // given
         var result: BoxOfficeDTO
-        let fileName = "box_office_sample"
-        
-        // when
-        guard let file = Bundle(for: JSONDeserializerTest.self).url(forResource: fileName, withExtension: "json") else {
-            throw TestError.fileNotFound
-        }
 
+        // when
         do {
-            let data = try Data(contentsOf: file)
+            let data = try Data(contentsOf: fileURL)
             result = try decoder.deserialize(data)
         } catch {
             throw error
@@ -47,5 +47,11 @@ final class JSONDeserializerTest: XCTestCase {
         XCTAssertEqual(result.boxOfficeResult.boxofficeType, "일별 박스오피스")
         XCTAssertEqual(result.boxOfficeResult.showRange, "20220105~20220105")
     }
-    
+
+    func findFile(name: String, withExtension fileExtension: String) -> URL? {
+
+        let fileURL = Bundle(for: JSONDeserializerTest.self).url(forResource: name, withExtension: fileExtension)
+
+        return fileURL
+    }
 }
