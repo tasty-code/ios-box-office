b96bb103079b87eb7d04d4808a459d818fb04399
Jason <ggj00125@gmail.com>
2023-04-28 11:53:24 +0900

    [Feat]: decode와 response에 대한 메서드 구현
    - verify(with:) HTTPRespense 에러 핸들링
    - decode(with:) Binary Data -> T 로 decoding하는 메서드

--- BoxOffice/Data/Network/NetworkService.swift
+++ BoxOffice/Data/Network/NetworkService.swift
@@ -14,26 +14,57 @@ class NetworkService {
     
     init(session: URLSession = URLSession(configuration: .default), endPoint: EndPoint) {
         self.session = session
-        self .endPoint = endPoint
+        self.endPoint = endPoint
     }
 
-    func request(from url: URL) async throws {
+    func request<T: Decodable>(from url: URL) async throws -> T {
         var result: Data?
-        var urlResponse: URLResponse?
+        var HTTPResponse: HTTPURLResponse?
 
         do {
             let (data, response) = try await session.data(for: endPoint.urlRequest(from: url))
             result = data
-            urlResponse = response as? HTTPURLResponse
-
-
+            HTTPResponse = response as? HTTPURLResponse
         } catch {
             throw NetworkError.urlRequest
         }
+        
+        guard let identifiedData = result else {
+            throw NetworkError.emptyData
+        }
+        
+        guard let identifiedResponse = HTTPResponse else {
+            throw NetworkError.unknownError
+        }
+        
+        try self.verify(with: identifiedResponse)
+        
+        return try self.decode(with: identifiedData)
     }
-
-    //TODO: - 디코딩하는 메서드 구현
-
-    //TODO: - URLResponse 에러 처리 메서드 구현
-
+    
+    func verify(with HTTPResponse: HTTPURLResponse) throws {
+        switch HTTPResponse.statusCode {
+        case (300...399):
+            throw HTTPErrorType.redirectionMessages(HTTPResponse.statusCode, HTTPResponse.debugDescription)
+        case (400...499):
+            throw HTTPErrorType.clientErrorResponses(HTTPResponse.statusCode, HTTPResponse.debugDescription)
+        case (500...599):
+            throw HTTPErrorType.serverErrorResponses(HTTPResponse.statusCode, HTTPResponse.debugDescription)
+        default:
+            throw HTTPErrorType.networkFailError(HTTPResponse.statusCode)
+        }
+    }
+    
+    func decode<T: Decodable>(with apiData: Data) throws -> T {
+        var decode: T
+        let decoder = JSONDecoder()
+        
+        do {
+            decode = try decoder.decode(T.self, from: apiData) as T
+        } catch {
+            throw NetworkError.decodeError
+        }
+        
+        return decode
+    }
 }
