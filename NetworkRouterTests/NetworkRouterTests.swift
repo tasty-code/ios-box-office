9784f738b66c61f1cc13413452c7463693c99774
Mason Kim <qwerty3345@naver.com>
2023-05-01 17:22:20 +0900

    [Test] Router 테스트 추가
    - test_네트워킹에_실패했을때_router의_completion이_Error를_던진다

---
+++ NetworkRouterTests/NetworkRouterTests.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkRouterTests.swift
+//  NetworkRouterTests
+//
+//  Created by Mason Kim on 2023/05/01.
+//
+
+import XCTest
+@testable import BoxOffice
+
+final class NetworkRouterTests: XCTestCase {
+    
+    var sut: NetworkRouter!
+
+    override func setUpWithError() throws {
+        
+    }
+
+    func test_네트워킹에_실패했을때_router의_completion이_Error를_던진다() {
+        let expectation = XCTestExpectation(description: "Networking")
+        
+        let session = MockURLSession(isFailRequest: true)
+        sut = NetworkRouter(session: session)
+        
+        let endpoint = MovieEndPoint.dailyBoxOffice(date: "231101")
+        
+        sut.request(endpoint) { result in
+            switch result {
+            case .success:
+                XCTFail("Mock URLSession이 실패하는 세션이기에 Success이면 안됨")
+            case .failure(let error):
+                XCTAssertEqual(error.localizedDescription,
+                               NetworkError.responseError(statusCode: 410).localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation])
+    }
+    
+    
+}
