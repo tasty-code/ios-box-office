f648f47546d20ab98d2ea9f23d7b2a825115cf7f
Mason Kim <qwerty3345@naver.com>
2023-05-01 11:03:05 +0900

    [Test] 영화 일일 박스오피스 샘플 파일 파싱 유닛 테스트 추가

--- BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
+++ BoxOfficeResponseParsingTests/BoxOfficeResponseParsingTests.swift
@@ -9,26 +9,44 @@ import XCTest
 
 final class BoxOfficeResponseParsingTests: XCTestCase {
     
-    var sut: Data!
+    var sutMovieDetail: Data!
+    var sutBoxOffice: Data!
     
     override func setUpWithError() throws {
-        let fileLocation = Bundle.main.url(forResource: "movie_detail_sample", withExtension: "json")!
+        let movieDetailFileLocation = Bundle.main.url(forResource: "movie_detail_sample", withExtension: "json")!
         XCTAssertNoThrow(
-            sut = try Data(contentsOf: fileLocation)
+            sutMovieDetail = try Data(contentsOf: movieDetailFileLocation)
+        )
+        
+        let boxOfficeFileLocation = Bundle.main.url(forResource: "box_office_sample", withExtension: "json")!
+        XCTAssertNoThrow(
+            sutBoxOffice = try Data(contentsOf: boxOfficeFileLocation)
         )
     }
     
     override func tearDownWithError() throws {
-        sut = nil
+        sutMovieDetail = nil
     }
     
-    func test_JSON파일의_Data를_디코딩했을때의_결과값이_비어있지_않다() throws {
+    func test_MovieDetail_JSON파일의_Data를_디코딩했을때의_결과값이_비어있지_않다() throws {
         // given
-        let data: Data = sut
+        let data: Data = sutMovieDetail
         
         // when
         let response = try JSONDecoder().decode(MovieDetailResponse.self, from: data)
         let decodedData = response.movieInfoResult.movieInfo.name
+        
+        // then
+        XCTAssertFalse(decodedData.isEmpty)
+    }
+    
+    func test_DailyBoxOffice_JSON파일의_Data를_디코딩했을때의_결과값이_비어있지_않다() throws {
+        // given
+        let data: Data = sutBoxOffice
+        
+        // when
+        let response = try JSONDecoder().decode(DailyBoxOfficeResponse.self, from: data)
+        let decodedData = response.boxOfficeResult.dailyBoxOfficeList
         
         // then
         XCTAssertFalse(decodedData.isEmpty)
