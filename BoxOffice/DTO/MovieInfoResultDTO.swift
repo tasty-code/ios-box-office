ca4801ec98d554692ceb4d84786ba1f16126619f
ohdair <ohdiar@gmail.com>
2023-05-01 10:32:59 +0900

    refactor: MovieInformationDTO 파일 분리

---
+++ BoxOffice/DTO/MovieInfoResultDTO.swift
@@ -0,0 +1,18 @@
+//
+//  MovieInfoResultDTO.swift
+//  BoxOffice
+//
+//  Created by 박재우 on 2023/05/01.
+//
+
+import UIKit
+
+struct MovieInfoResultDTO {
+    let movieInformation: MovieInformationDTO
+    let source: String
+
+    enum CodingKeys: String, CodingKey {
+        case movieInformation = "movieInfo"
+        case source
+    }
+}
