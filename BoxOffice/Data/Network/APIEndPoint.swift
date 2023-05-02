2cfc411726d484c741fc32045ab81397dca5f18c
Lust3r <s1skyblue1s@gmail.com>
2023-05-02 10:05:39 +0900

    [Refactor]: APIEndPoint에 EndPoint를 반환하는 receive 메서드 구현
    - 기존에 수행하던 url 관련 메서드는 Requestable.swift에서 수행
    - receiveBoxOffice(with:) 메서드를 통해 일일 박스오피스 EndPoint 반환
    - receiveMovieDetailInformation(with:) 메서드를 통해 영화 상세정보 EndPoint 반환

--- BoxOffice/Data/Network/APIEndPoint.swift
+++ BoxOffice/Data/Network/APIEndPoint.swift
@@ -9,36 +9,11 @@ import Foundation
 
 struct APIEndPoint {
 
-    private(set) var convertedData: String
-
-    func urlRequest(from movieURL: URL) throws -> URLRequest {
-
-        var urlRequest = URLRequest(url: movieURL)
-        urlRequest.httpMethod = HTTPMethodType.get.rawValue
-
-        return urlRequest
+    static func receiveBoxOffice(with boxOfficeRequestDTO: BoxOfficeRequestDTO) -> EndPoint<BoxOffice> {
+        return EndPoint(baseURL: Basic.baseURL, firstPath: Show.boxOffice, secondPath: Show.searchDailyList, queryParameters: boxOfficeRequestDTO)
     }
 
-    func url(with path: URLComponents) throws -> URL {
-
-        var makedPath = path
-        let key = URLQueryItem(name: APIMagicLiteral.Key, value: APIMagicLiteral.KeyValue)
-        let targetDate = URLQueryItem(name: APIMagicLiteral.targetQuery, value: convertedData)
-        makedPath.queryItems = [key, targetDate]
-        
-        guard let url = makedPath.url else {
-            throw URLComponentsError.invalidComponent
-        }
-        
-        return url
-    }
-    
-    func makePath(with firstPath: String, and lastPath: String) throws -> URLComponents {
-
-        guard let components = URLComponents(string: "\(Basic.baseURL)\(firstPath)\(lastPath)\(Basic.format)") else {
-            throw URLComponentsError.invalidComponent
-        }
-
-        return components
+    static func receiveMovieDetailInformation(with movieDetailRequestDTO: MovieDetailRequestDTO) -> EndPoint<MovieDetailInformation> {
+        return EndPoint(baseURL: Basic.baseURL, firstPath: Movie.category, secondPath: Movie.searchMovieInfo, queryParameters: movieDetailRequestDTO)
     }
 }
