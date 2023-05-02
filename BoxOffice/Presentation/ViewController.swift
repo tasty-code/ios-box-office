a7128bae38b3a4b305a7e4d34b14b0982ba8e668
Jason <ggj00125@gmail.com>
2023-05-02 10:43:31 +0900

    [Feat] 네트워킹 타입 구현한 내용을 출력하기 위한 코드 작성

--- BoxOffice/Presentation/ViewController.swift
+++ BoxOffice/Presentation/ViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let networkService = NetworkService()
+    let boxOfficeDTO = BoxOfficeRequestDTO(targetDate: "20230501")
+    let moiveDetailDTO = MovieDetailRequestDTO(movieCode: "20226270")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Task {
+            let boxOfficeResult = try await networkService.request(with: APIEndPoint.receiveBoxOffice(with: boxOfficeDTO))
+            print(boxOfficeResult)
+            
+            let movieDetailResult = try await networkService.request(with: APIEndPoint.receiveMovieDetailInformation(with: moiveDetailDTO))
+            print(movieDetailResult)
+        }
     }
 
 }
