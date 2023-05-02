cd16750e201ad09a17380bab3e483293b786e84c
Lust3r <s1skyblue1s@gmail.com>
2023-05-02 15:46:44 +0900

    [Feat] 영화 제목과 관객 수 레이블을 담을 BoxOfficeBrief 클래스 초기 구현

---
+++ BoxOffice/Presentation/Cells/BoxOfficeBrief.swift
@@ -0,0 +1,18 @@
+//
+//  BoxOfficeBrief.swift
+//  BoxOffice
+//
+//  Created by 이상윤 on 2023/05/02.
+//
+
+import UIKit
+
+final class BoxOfficeBrief: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
