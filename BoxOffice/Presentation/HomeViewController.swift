b456eb0ae05702233992b87eef3ed65c008d6546
Jason <ggj00125@gmail.com>
2023-05-02 13:42:02 +0900

    [Feat] navigationBar 구현 및 적용

--- BoxOffice/Presentation/HomeViewController.swift
+++ BoxOffice/Presentation/HomeViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let navigationBar : UINavigationBar = {
+        let navigationBar = UINavigationBar()
+        let navigationItem = UINavigationItem(title: "2023-05-01")
+        navigationBar.setItems([navigationItem], animated: true)
+        
+        return navigationBar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureOfMainViewLayout()
     }
     
     //MARK: - Private Property
@@ -23,19 +32,29 @@ class HomeViewController: UIViewController {
         
         //TODO: - Register of CollectionViewCell
         
-        configureOfMainViewLayout()
-        
         return collectionView
     }()
     
     //MARK: - Private Method
     private func configureOfMainViewLayout() {
-        self.view.addSubview(collectionView)
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        self.view.backgroundColor = .white
+        self.view.addSubview(navigationBar)
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            navigationBar.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            navigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+        
+        self.view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: navigationBar.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor)
         ])
     }
 }
