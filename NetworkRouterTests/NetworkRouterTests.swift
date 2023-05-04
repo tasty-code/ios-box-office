965ba7b92aa8ce66e2946f01fcd0280b787579b5
Mason Kim <qwerty3345@naver.com>
2023-05-04 10:58:41 +0900

    [Test] NetworkRouter의 cancel 에 대한 테스트 추가
    - test_request_직후_cancel을_했을때_작업이_취소된다

--- NetworkRouterTests/NetworkRouterTests.swift
+++ NetworkRouterTests/NetworkRouterTests.swift
@@ -44,4 +44,36 @@ final class NetworkRouterTests: XCTestCase {
         
         wait(for: [expectation], timeout: 1)
     }
+    
+    func test_request_직후_cancel을_했을때_작업이_취소된다() {
+        
+        // given
+        let expectation = XCTestExpectation()
+        let endpoint = MockEndpoint()
+        let urlSession = MockURLSession(isFailRequest: true)
+        sut = NetworkRouter(session: urlSession)
+        
+        sut.request(endpoint) { (result: Result<Data, NetworkError>) in
+
+            // then
+            switch result {
+            case .success:
+                XCTFail("Mock URLSession이 실패하는 세션이기에 Success이면 안됨")
+            case .failure(let error):
+                guard case .transportError(let error) = error else {
+                    XCTFail("Cancel 에 대한 error 를 transport 해야 함")
+                    return
+                }
+                let nsError = error as NSError
+                XCTAssertEqual(nsError.domain, NSURLErrorDomain)
+                XCTAssertEqual(nsError.code, NSURLErrorCancelled)
+            }
+            expectation.fulfill()
+        }
+        
+        // when
+        sut.cancel()
+        
+        wait(for: [expectation], timeout: 1)
+    }
 }
