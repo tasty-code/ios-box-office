aaaea2f8009c8d2ec6b4398f4843b6aa8dc93dbb
ueunli <cdenen20@gmail.com>
2023-05-04 10:49:38 +0900

    Refactor: api 요청 메서드 로직 수정
    - 컴플리션 핸들러의 매개변수를 Result타입으로 변경
    - NetworkError에 LocalizedError프로토콜 채택

--- BoxOffice/Network/NetworkService.swift
+++ BoxOffice/Network/NetworkService.swift
@@ -9,29 +9,57 @@ import Foundation
 
 struct NetworkService {
     
-    static func fetchBoxOfficeData(date: String, completion: @escaping (DailyBoxOfficeDTO?) -> Void) {
+    static func fetchBoxOfficeData(date: String, completion: @escaping (Result<DailyBoxOfficeDTO, Error>) -> Void) {
         URLSession.shared.dataTask(with: API.boxofficeMovie(date: date).urlRequest) { data, response, error in
-            guard error == nil else { return }
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
             
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
-                  (200 ..< 300).contains(statusCode) else { return }
+                  (200 ..< 300).contains(statusCode) else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
             
-            guard let data = data else { return }
-            let parsedData = try? JSONDecoder().decode(DailyBoxOfficeDTO.self, from: data)
-            completion(parsedData)
+            guard let data = data else {
+                completion(.failure(NetworkError.dataNotFound))
+                return
+            }
+            
+            guard let parsedData = try? JSONDecoder().decode(DailyBoxOfficeDTO.self, from: data) else {
+                completion(.failure(NetworkError.dataDecodingFailed))
+                return
+            }
+            
+            completion(.success(parsedData))
         }.resume()
     }
     
-    static func fetchMovieData(code: String, completion: @escaping (MovieDetailDTO?) -> Void) {
+    static func fetchMovieData(code: String, completion: @escaping (Result<MovieDetailDTO, Error>) -> Void) {
         URLSession.shared.dataTask(with: API.movie(code: code).urlRequest) { data, response, error in
-            guard error == nil else { return }
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
             
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
-                  (200 ..< 300).contains(statusCode) else { return }
+                  (200 ..< 300).contains(statusCode) else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
             
-            guard let data = data else { return }
-            let parsedData = try? JSONDecoder().decode(MovieDetailDTO.self, from: data)
-            completion(parsedData)
+            guard let data = data else {
+                completion(.failure(NetworkError.dataNotFound))
+                return
+            }
+            
+            guard let parsedData = try? JSONDecoder().decode(MovieDetailDTO.self, from: data) else {
+                completion(.failure(NetworkError.dataDecodingFailed))
+                return
+            }
+            
+            completion(.success(parsedData))
         }.resume()
     }
     
