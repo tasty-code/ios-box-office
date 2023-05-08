4c2f96d5412bb454cd8078f084e7d173ce00916e
longlivedrgn <kimye9808@naver.com>
2023-05-08 15:16:06 +0900

    feat: navigation controller 임베드 및 collection view 넣고, compositionalLayout 설정해주기

--- BoxOffice/Scenes/DailyBoxOfficeViewController.swift
+++ BoxOffice/Scenes/DailyBoxOfficeViewController.swift
@@ -9,8 +9,42 @@ import UIKit
 
 final class DailyBoxOfficeViewController: UIViewController {
 
+    enum Section {
+        case main
+    }
+
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, DailyBoxOffice>
+    typealias SnapShot = NSDiffableDataSourceSnapshot<Section, DailyBoxOffice>
+
+    private let boxOfficeManager = BoxOfficeAPIManager()
+    private var dataSource: DataSource?
+    private var dailyBoxOfficeCollectionView: UICollectionView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUp()
+        configureCollectionView()
+    }
+
+    private func setUp() {
+        navigationItem.title = "데이터 받아오는 중~!"
+        view.backgroundColor = .systemBackground
+    }
+
+    private func configureCollectionView() {
+        dailyBoxOfficeCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        guard let dailyBoxOfficeCollectionView = dailyBoxOfficeCollectionView else { return }
+        view.addSubview(dailyBoxOfficeCollectionView)
+    }
+
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { section, layoutEnvironment in
+            let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+
+            return NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: layoutEnvironment)
+        }
+
+        return layout
     }
 
 }
