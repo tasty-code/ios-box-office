136676a5b5f21cc8f157156f57c194e68204816b
Lust3r <s1skyblue1s@gmail.com>
2023-05-09 11:30:13 +0900

    [Feat]: API 호출 데이터 UI 출력

--- BoxOffice/Presentation/HomeViewController.swift
+++ BoxOffice/Presentation/HomeViewController.swift
@@ -8,25 +8,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
+
     let networkService = NetworkService()
-    
+    var testEntity = [DailyBoxOffice]()
+
     //MARK: - Initializer
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         configureHierarchy()
         configureDataSource()
+        initialSnapShot()
+        fetchData()
     }
-    
+
     //MARK: - Private Property
-    
+
     private let navigationBar : UINavigationBar = {
         let navigationBar = UINavigationBar()
         let navigationItem = UINavigationItem(title: "2023-05-01")
         navigationBar.setItems([navigationItem], animated: true)
-        
+
         return navigationBar
     }()
 
@@ -36,22 +39,31 @@ class HomeViewController: UIViewController {
         collectionView.showsVerticalScrollIndicator = true
         collectionView.clipsToBounds = true
         collectionView.backgroundColor = .systemGray2
-        
+
         return collectionView
     }()
-    
+
     private var dataSource: UICollectionViewDiffableDataSource<Section, DailyBoxOffice>!
-    
-    private func fetchData() -> BoxOfficeResult? {
+
+    private func fetchData() {
         let boxOfficeRequestDTO = BoxOfficeRequestDTO(targetDate: "20230507")
-        
+
         var networkResult: BoxOfficeResult?
-        
+
         Task {
             let result = try await networkService.request(with: APIEndPoint.receiveBoxOffice(with: boxOfficeRequestDTO))
             networkResult = result.boxOfficeResult
+            networkResult?.dailyBoxOfficeList.forEach({ officeList in
+                testEntity.append(DailyBoxOffice(rankEmoji: UIImage(systemName: "heart.fill")!, movieBrief: MovieBrief(movieName: officeList.movieName, audienceCount: officeList.audienceCount), rank: Rank(rank: officeList.rank, movieType: officeList.rankVariation)))
+            })
+            applySnapshot(items: testEntity, section: Section.main)
         }
-        return networkResult
+    }
+
+    private func applySnapshot(items: [DailyBoxOffice], section: Section) {
+        var snapshot = dataSource.snapshot()
+        snapshot.appendItems(items, toSection: section)
+        dataSource.apply(snapshot)
     }
 }
 
@@ -59,7 +71,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController {
     private func configureHierarchy() {
         let safeArea = self.view.safeAreaLayoutGuide
-        
+
         self.view.backgroundColor = .white
         self.view.addSubview(navigationBar)
         navigationBar.translatesAutoresizingMaskIntoConstraints = false
@@ -68,7 +80,7 @@ extension HomeViewController {
             navigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             navigationBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
         ])
-        
+
         self.view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -78,7 +90,7 @@ extension HomeViewController {
             collectionView.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor)
         ])
     }
-    
+
     private func configureOfCollectionViewLayout() -> UICollectionViewLayout {
         let configure = UICollectionLayoutListConfiguration(appearance: .plain)
         let layout = UICollectionViewCompositionalLayout.list(using: configure)
@@ -97,7 +109,7 @@ extension HomeViewController {
             cell.boxOfficeRank.setRankVariation(by: dailyBoxOffice.rankEmoji)
             cell.boxOfficeRank.setRank(by: dailyBoxOffice.rank.rank)
             cell.boxOfficeRank.setRankVaritaion(by: .red)
-            
+
             cell.reloadInputViews()
         }
 
@@ -105,33 +117,19 @@ extension HomeViewController {
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: dailyBoxOffice)
         }
 
-        let snapshot = initialSnapShot()
-        dataSource.apply(snapshot, animatingDifferences: true)
     }
 
-    func initialSnapShot() -> NSDiffableDataSourceSnapshot<Section, DailyBoxOffice> {
-        
+    func initialSnapShot() {
+
 //        let testEntity = [
 //            DailyBoxOffice(rankEmoji: UIImage(), movieBrief: MovieBrief(movieName: "경관의 피", audienceCount: "오늘 64,050 / 총 69,228"), rank: Rank(rank: "1", movieType: "신작")),
 //            DailyBoxOffice(rankEmoji: UIImage(systemName: "heart.fill") ?? UIImage(), movieBrief: MovieBrief(movieName: "영화 2", audienceCount: "123"), rank: Rank(rank: "123", movieType: "456")),
 //            DailyBoxOffice(rankEmoji: UIImage(systemName: "star.fill") ?? UIImage(), movieBrief: MovieBrief(movieName: "영화 3", audienceCount: "123"), rank: Rank(rank: "123", movieType: "456"))
 //        ]
-        
-        var testEntity = [DailyBoxOffice]()
-        
-        if let result = fetchData() {
-            result.dailyBoxOfficeList.forEach { dailyBoxOfficeList in
-                let data = DailyBoxOffice(rankEmoji: UIImage(systemName: "heart.fill")!,
-                               movieBrief: MovieBrief(movieName: dailyBoxOfficeList.movieName, audienceCount: dailyBoxOfficeList.audienceCount),
-                               rank: Rank(rank: dailyBoxOfficeList.rank, movieType: dailyBoxOfficeList.openDate))
-                testEntity.append(data)
-            }
-        }
 
         var snapshot = NSDiffableDataSourceSnapshot<Section, DailyBoxOffice>()
         snapshot.appendSections([.main])
         snapshot.appendItems(testEntity)
-
-        return snapshot
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
 }
