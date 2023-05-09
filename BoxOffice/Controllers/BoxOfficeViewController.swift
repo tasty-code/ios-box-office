661b855595d8646100e6350cc20be2f8fa2b185b
doshkor <dosh.kor@gmail.com>
2023-05-09 11:43:02 +0900

    Feat: movieCollectionView 생성

--- BoxOffice/Controllers/BoxOfficeViewController.swift
+++ BoxOffice/Controllers/BoxOfficeViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class BoxOfficeViewController: UIViewController {
     
+    // MARK: - Private
     private let networkService = NetworkService()
     private var dailyBoxOffice: DailyBoxOfficeDTO?
     private var movieDetail: MovieDetailDTO?
     
+    private var movieCollectionView: UICollectionView! = nil
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,9 +25,28 @@ class BoxOfficeViewController: UIViewController {
             self.dailyBoxOffice = $0
         }
         
+        setup()
     }
     
-    func updateData<T: APIRepresentable, E: EndPointProtocol>(from api: E, completion: @escaping (T?) -> Void) where E.DTO == T {
+    // MARK: - Private function
+    private func setup() {
+        configureCollectionView()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.addSubview(movieCollectionView)
+    }
+        
+    private func configureCollectionView() {
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemPink
+        movieCollectionView = collectionView
+    }
+    
+    private func updateData<T: APIRepresentable, E: EndPointProtocol>(from api: E, completion: @escaping (T?) -> Void) where E.DTO == T {
         networkService.fetchData(endPoint: api) { result in
             switch result {
             case .success(let data):
