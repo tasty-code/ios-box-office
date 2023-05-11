3e9dc9638b963b51ddc2e651919e4c542c613370
Lust3r <s1skyblue1s@gmail.com>
2023-05-11 10:07:07 +0900

    [Refactor]: 줄바꿈을 통한 EndPoint의 파라미터 가독성 증진

--- BoxOffice/Data/Network/APIEndPoint.swift
+++ BoxOffice/Data/Network/APIEndPoint.swift
@@ -10,10 +10,16 @@ import Foundation
 struct APIEndPoint {
 
     static func receiveBoxOffice(with boxOfficeRequestDTO: BoxOfficeQueryParameters) -> EndPoint<BoxOffice> {
-        return EndPoint(baseURL: Basic.baseURL, firstPath: Show.boxOffice, secondPath: Show.searchDailyList, queryParameters: boxOfficeRequestDTO)
+        return EndPoint(baseURL: Basic.baseURL,
+                        firstPath: Show.boxOffice,
+                        secondPath: Show.searchDailyList,
+                        queryParameters: boxOfficeRequestDTO)
     }
 
     static func receiveMovieDetailInformation(with movieDetailRequestDTO: MovieDetailQueryParameters) -> EndPoint<MovieDetailInformation> {
-        return EndPoint(baseURL: Basic.baseURL, firstPath: Movie.category, secondPath: Movie.searchMovieInfo, queryParameters: movieDetailRequestDTO)
+        return EndPoint(baseURL: Basic.baseURL,
+                        firstPath: Movie.category,
+                        secondPath: Movie.searchMovieInfo,
+                        queryParameters: movieDetailRequestDTO)
     }
 }
