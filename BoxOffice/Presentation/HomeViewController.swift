c5c0c6c6fc06ab361b948b174502c2fd3abc7fd5
Jason <ggj00125@gmail.com>
2023-05-11 13:12:43 +0900

    [Style]: 필요없는 변수 삭제

--- BoxOffice/Presentation/HomeViewController.swift
+++ BoxOffice/Presentation/HomeViewController.swift
@@ -78,12 +78,10 @@ class HomeViewController: UIViewController {
         let yesterdayDate = formatter.receiveCurrentDate().split(separator: "-").joined()
         let boxOfficeQueryParameters = BoxOfficeQueryParameters(targetDate: yesterdayDate)
 
-        var networkResult: BoxOfficeResult?
-
         Task {
             let result = try await networkService.request(with: APIEndPoint.receiveBoxOffice(with: boxOfficeQueryParameters))
-            networkResult = result.boxOfficeResult
-            networkResult?.dailyBoxOfficeList.forEach({ officeList in
+            
+            result.boxOfficeResult.dailyBoxOfficeList.forEach({ officeList in
                 dailyBoxOfficeStorage.append(DailyBoxOffice(movieBrief: MovieBrief(movieName: officeList.movieName, audienceCount: officeList.audienceCount, audienceAccumulated: officeList.audienceAccumulate), rank: Rank(rank: officeList.rank, rankVariation: officeList.rankVariation, rankOldAndNew: officeList.rankOldAndNew)))
             })
             applySnapshot()
