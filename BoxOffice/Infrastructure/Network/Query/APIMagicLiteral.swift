ecf1cbf3e820988996666f0fdfdb9ef459bee706
Lust3r <s1skyblue1s@gmail.com>
2023-05-11 10:16:47 +0900

    [Refactor]: APIMagicLiteral에서 APIKEY 값 제거

--- BoxOffice/Infrastructure/Network/Query/APIMagicLiteral.swift
+++ BoxOffice/Infrastructure/Network/Query/APIMagicLiteral.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 enum APIMagicLiteral {
-    static let key = "f5eef3421c602c6cb7ea224104795888"
     static let targetQuery = "targetDt"
     static let movieCode = "movieCd"
 }
