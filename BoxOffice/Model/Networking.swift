84ecb672af91e8123c3fbdf8831153adbf7d3336
ohdair <ohdiar@gmail.com>
2023-05-11 17:31:35 +0900

    feat: Poster를 이미지로 가져올 수 있도록 Networking loadImage 메서드 추가

--- BoxOffice/Model/Networking.swift
+++ BoxOffice/Model/Networking.swift
@@ -8,32 +8,64 @@
 import UIKit
 
 class Networking {
-    func loadData<T: Convertable>(_ method: T.Type, request: URLRequest, completion: @escaping (MovieDatable?, Error?) -> Void) {
+    func loadData(_ method: EndPoint, completion: @escaping (MovieDatable?, Error?) -> Void) {
+        do {
+            let urlRequest = try method.makeRequest()
 
-        URLSession.shared.dataTask(with: request) { data, response, error in
+            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                guard error == nil else {
+                    completion(nil, NetworkError.transportError)
+                    return
+                }
+
+                guard let response = response as? HTTPURLResponse,
+                      (200...299).contains(response.statusCode) else {
+                    completion(nil, NetworkError.serverError)
+                    return
+                }
+
+                guard let safeData = data else {
+                    completion(nil, NetworkError.missingData)
+                    return
+                }
+
+                guard let decodedData = self.loadJSON(method.convertType, data: safeData) as? Convertable else {
+                    completion(nil, NetworkError.decodingError)
+                    return
+                }
+
+                completion(decodedData.convert(), nil)
+            }.resume()
+        } catch {
+            completion(nil, error)
+        }
+    }
+
+    func loadImage(_ method: EndPoint, completion: @escaping (UIImage?, Error?) -> Void) {
+        loadData(method) { poster, error in
             guard error == nil else {
-                completion(nil, NetworkError.transportError)
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse,
-                  (200...299).contains(response.statusCode) else {
-                completion(nil, NetworkError.serverError)
+                completion(nil, error)
                 return
             }
 
-            guard let safeData = data else {
-                completion(nil, NetworkError.missingData)
-                return
-            }
-
-            guard let decodedData = self.loadJSON(T.self, data: safeData) as? T else {
+            guard let poster = poster as? MoviePoster else {
                 completion(nil, NetworkError.decodingError)
                 return
             }
 
-            completion(decodedData.convert(), nil)
-        }.resume()
+            let posterURL = poster.url.replacingOccurrences(of: "http://", with: "https://")
+
+            guard let url = URL(string: posterURL) else {
+                completion(nil, NetworkError.decodingError)
+                return
+            }
+
+            print(#line, url)
+
+            if let data = try? Data(contentsOf: url) {
+                completion(UIImage(data: data), nil)
+            }
+        }
     }
 
     private func loadJSON<T: Convertable>(_ method: T.Type, data: Data) -> Decodable? {
