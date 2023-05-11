73d5f3e6039895da621d600301f4ba6c5670601a
ohdair <ohdiar@gmail.com>
2023-05-11 17:16:53 +0900

    feat: MoviePoster 데이터 받을 객체 추가

---
+++ BoxOffice/Model/MoviePoster/MoviePoster.swift
@@ -0,0 +1,13 @@
+//
+//  MoviePoster.swift
+//  BoxOffice
+//
+//  Created by 박재우 on 2023/05/11.
+//
+
+import UIKit
+
+struct MoviePoster: MovieDatable {
+    var url: String
+    var image: UIImage?
+}
