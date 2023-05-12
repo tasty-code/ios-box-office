e3993fe9a3914cf9374ee47cc10ef371d9d2d344
Jason <ggj00125@gmail.com>
2023-05-12 15:58:34 +0900

    [Feat]: Notification.Name extension으로 정적 상수 추가

---
+++ BoxOffice/Presentation/ViewModel/NotificationCenter + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationCenter + Extension.swift
+//  BoxOffice
+//
+//  Created by Jason on 2023/05/12.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let loadedBoxOfficeData = Notification.Name("loadedBoxOfficeData")
+}
