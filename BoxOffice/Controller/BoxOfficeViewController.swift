992b65a39c53b636ac136e9d897d6364575f5c82
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-14 22:00:52 +0900

    feat: 첫 화면 시작 시 로드 중임을 알 수 있도록 하는 Indicator 구현
    - `UIActivityIndicatorView`를 채택한 `ActivityIndicatorView` 생성
    - `UIVerticalStackView`와 `UIActivityIndicatorView`에 final 키워드 추가
    - `BoxOfficeViewController`에 indicator 추가

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -12,6 +12,7 @@ class BoxOfficeViewController: UIViewController {
     private var dataSource: UICollectionViewDataSource?
 
     private lazy var collectionView = BoxOfficeCollectionView(frame: .zero)
+    private lazy var indicatorView = ActivityIndicatorView(frame: view.bounds)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,7 @@ class BoxOfficeViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         view.addSubview(collectionView)
+        view.addSubview(indicatorView)
         
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.topAnchor),
