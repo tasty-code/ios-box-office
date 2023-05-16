203c99f6b38ca0ace811e351e833f4411abb6e7c
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-16 17:33:10 +0900

    feat: MovieInformationItem 타입 구현

---
+++ BoxOffice/Model/PresentationModel/Implementation/MovieInformation/MovieInformationItem.swift
@@ -0,0 +1,46 @@
+//
+//  MovieInformationItem.swift
+//  BoxOffice
+//
+//  Created by Blu on 2023/05/16.
+//
+
+import UIKit
+
+struct MovieInformationItem: ViewDisplayable {
+
+    let movieName: String
+    let poster: UIImage?
+    let director: String
+    let productionYear: String
+    let openDate: String
+    let showTime: String
+    let auditGrade: String
+    let nation: String
+    let genre: String
+    let actor: String
+
+    init(movieName: String, poster: UIImage?, directors: [DirectorDTO], productionYear: String, openDate: String, showTime: String, audits: [AuditDTO], nations: [NationDTO], genres: [GenreDTO], actors: [ActorDTO]) {
+
+        self.movieName = movieName
+        self.poster = poster
+        self.productionYear = productionYear
+        self.openDate = openDate
+        self.showTime = showTime
+
+        let director = directors.first?.peopleNm ?? ""
+        self.director = director
+
+        let auditGrade = audits.first?.watchGradeNm ?? ""
+        self.auditGrade = auditGrade
+
+        let nation = nations.first?.nationNm ?? ""
+        self.nation = nation
+
+        let genre = genres.map { $0.genreNm }.joined(separator: ", ")
+        self.genre = genre
+
+        let actor = actors.map { $0.peopleNm }.joined(separator: ", ")
+        self.actor = actor
+    }
+}
