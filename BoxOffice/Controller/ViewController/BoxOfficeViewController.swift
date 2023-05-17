71135e12cbfe87f9a3d093ca00236b5baf960eb5
SunnnySong <tjsghktjswls@naver.com>
2023-05-17 11:17:17 +0900

    refactor: BoxOfficeViewController BoxOffices 로드 코드 수정
    - setupInitialBoxOffices 함수 생성 : 초기 BoxOffices 데이터 설정 및

--- BoxOffice/Controller/ViewController/BoxOfficeViewController.swift
+++ BoxOffice/Controller/ViewController/BoxOfficeViewController.swift
@@ -11,22 +11,19 @@ class BoxOfficeViewController: UIViewController {
     
     private var dataSource: UICollectionViewDataSource?
     private let presentationProvider = PresentationProvider()
-
+    
     private var boxOffices: [BoxOfficeItem] {
         self.presentationProvider.getBoxOffices()
     }
-
+    
     private lazy var collectionView = BoxOfficeCollectionView(frame: .zero)
     private lazy var indicatorView = ActivityIndicatorView(frame: view.bounds)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = presentationProvider.getBoxOfficeDate()
-        presentationProvider.loadBoxOffices()
-
+        
         configureHierarchy()
-        configureDataSource()
+        setupInitialBoxOffices()
         configureRefreshControl()
     }
     
@@ -44,16 +41,25 @@ class BoxOfficeViewController: UIViewController {
         ])
     }
     
-    private func configureDataSource() {
+    private func setupInitialBoxOffices() {
+        
+        presentationProvider.loadBoxOffices()
+        configureDataSource {
+            self.indicatorView.stopAnimating()
+        }
+    }
+    
+    private func configureDataSource(completionHandler: @escaping (() -> Void)) {
 
         presentationProvider.boxOfficeCall = {
             let boxOfficeDataSource = BoxOfficeDataSource()
-            boxOfficeDataSource.boxOffices = self.presentationProvider.getBoxOffices()
+            boxOfficeDataSource.boxOffices = self.boxOffices
             self.dataSource = boxOfficeDataSource
 
             DispatchQueue.main.async {
+                self.title = self.presentationProvider.getBoxOfficeDate()
                 self.collectionView.dataSource = self.dataSource
-                self.indicatorView.stopAnimating()
+                completionHandler()
             }
         }
     }
@@ -71,17 +77,10 @@ extension BoxOfficeViewController {
         self.presentationProvider.loadBoxOffices()
         self.collectionView.isScrollEnabled = false
 
-        presentationProvider.boxOfficeCall = {
-            let boxOfficeDataSource = BoxOfficeDataSource()
-            boxOfficeDataSource.boxOffices = self.presentationProvider.getBoxOffices()
-            self.dataSource = boxOfficeDataSource
-
-            DispatchQueue.main.async {
-                self.collectionView.dataSource = self.dataSource
-                self.collectionView.reloadData()
-                self.collectionView.refreshControl?.endRefreshing()
-                self.collectionView.isScrollEnabled = true
-            }
+        configureDataSource {
+            self.collectionView.reloadData()
+            self.collectionView.refreshControl?.endRefreshing()
+            self.collectionView.isScrollEnabled = true
         }
     }
 }
---
+++ BoxOffice/Controller/ViewController/MovieInformationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieInformationViewController.swift
+//  BoxOffice
+//
+//  Created by Sunny on 2023/05/17.
+//
+
+import UIKit
+
+class MovieInformationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
