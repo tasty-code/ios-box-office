034e83a0e9d8a6aaf062c0d27bd74c765e09e5a4
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-13 13:13:46 +0900

    feat: NetworkError 타입 구현

---
+++ BoxOffice/Model/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  BoxOffice
+//
+//  Created by 강창현 on 2/13/24.
+//
+
+enum NetworkError: Error {
+    case redirectionError
+    case clientError
+    case serverError
+}
