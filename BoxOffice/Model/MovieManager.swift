a09d7aabd8cd26548137497a2548e0f54bcae44f
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-13 16:49:39 +0900

    refactor: fetchMovie() Result 타입을 활용한 error 핸들링

--- BoxOffice/Model/MovieManager.swift
+++ BoxOffice/Model/MovieManager.swift
@@ -8,35 +8,46 @@
 import Foundation
 
 final class MovieManager {
-    var movieData: Movie?
-    func fetchMovie() {
+    private var movieData: Movie?
+}
+
+extension MovieManager {
+    typealias NetworkResult = (Result<Movie,NetworkError>)
+    func fetchMovie(date: String, completion: @escaping (NetworkResult) -> ()) -> Movie? {
         guard
-            let url: URL = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json")
+            let url: URL = URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=\(date)")
         else {
-            return
+            completion(.failure(.invalidURLError))
+            return nil
         }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let error else { return }
-            guard let response = response as? HTTPURLResponse else { return }
+            guard error == nil else { return }
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(.invalidResponseError))
+                return
+            }
             
             switch response.statusCode {
             case 300..<400:
-                return print(NetworkError.redirectionError)
+                return completion(.failure(.redirectionError))
             case 400..<500:
-                return print(NetworkError.clientError)
+                return completion(.failure(.clientError))
             case 500..<600:
-                return print(NetworkError.serverError)
+                return completion(.failure(.serverError))
             default:
                 break
             }
             
             guard let data = data else { return }
-            do {
-                self.movieData = try JSONDecoder().decode(Movie.self, from: data)
-            } catch {
-                print(error.localizedDescription)
+            DispatchQueue.main.async {
+                do {
+                    self.movieData = try JSONDecoder().decode(Movie.self, from: data)
+                } catch {
+                    completion(.failure(.decodingError))
+                }
             }
-        }
+        }.resume()
+        return movieData
     }
 }
