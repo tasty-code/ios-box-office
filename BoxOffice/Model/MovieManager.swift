e8f706374bacdf5ae482f68b43ead105ac086a4a
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-13 13:14:17 +0900

    feat: MovieManager URLSession 기능 구현

--- BoxOffice/Model/MovieManager.swift
+++ BoxOffice/Model/MovieManager.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+final class MovieManager {
+    var movieData: Movie?
+    func fetchMovie() {
+        guard
+            let url: URL = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json")
+        else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let error else { return }
+            guard let response = response as? HTTPURLResponse else { return }
+            
+            switch response.statusCode {
+            case 300..<400:
+                return print(NetworkError.redirectionError)
+            case 400..<500:
+                return print(NetworkError.clientError)
+            case 500..<600:
+                return print(NetworkError.serverError)
+            default:
+                break
+            }
+            
+            guard let data = data else { return }
+            do {
+                self.movieData = try JSONDecoder().decode(Movie.self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
