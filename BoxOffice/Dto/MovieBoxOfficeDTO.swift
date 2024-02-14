cfbeb80600465b61c9ec397e31d6275c722cda6e
jus1234 <osjamg1996@gmail.com>
2024-02-14 19:26:23 +0900

    chore: MovieBoxOfficeDTO 테스트용 코드 제거

--- BoxOffice/Dto/MovieBoxOfficeDTO.swift
+++ BoxOffice/Dto/MovieBoxOfficeDTO.swift
@@ -18,56 +18,10 @@ struct MovieBoxOfficeDTO: Decodable {
     let audienceAccumulation: String
     let screenCount: String
     let showingCount: String
-    
-    init(rank: String, rankChangesWithPreviousDay: String, rankOldAndNew: String, movieCode: String, movieName: String, openingDate: String, salesAmount: String, salesShare: String, salesChangesWithPreviousDay: String, salesChangesRatioWithPreviousDay: String, salesAccumulation: String, audienceCount: String, audienceCountChangesWithPreviousDay: String, audienceCountChangesRatioWithPreviousDay: String, audienceAccumulation: String, screenCount: String, showingCount: String) {
-        self.rank = rank
-        self.rankChangesWithPreviousDay = rankChangesWithPreviousDay
-        self.rankOldAndNew = rankOldAndNew
-        self.movieCode = movieCode
-        self.movieName = movieName
-        self.openingDate = openingDate
-        self.salesAmount = salesAmount
-        self.salesShare = salesShare
-        self.salesChangesWithPreviousDay = salesChangesWithPreviousDay
-        self.salesChangesRatioWithPreviousDay = salesChangesRatioWithPreviousDay
-        self.salesAccumulation = salesAccumulation
-        self.audienceCount = audienceCount
-        self.audienceCountChangesWithPreviousDay = audienceCountChangesWithPreviousDay
-        self.audienceCountChangesRatioWithPreviousDay = audienceCountChangesRatioWithPreviousDay
-        self.audienceAccumulation = audienceAccumulation
-        self.screenCount = screenCount
-        self.showingCount = showingCount
-    }
-}
-
-extension MovieBoxOfficeDTO {
-    enum CodingKeys: String, CodingKey {
-        case rank = "rank"
-        case rankChangesWithPreviousDay = "rankInten"
-        case rankOldAndNew = "rankOldAndNew"
-        case movieCode = "movieCd"
-        case movieName = "movieNm"
-        case openingDate = "openDt"
-        case salesAmount = "salesAmt"
-        case salesShare = "salesShare"
-        case salesChangesWithPreviousDay = "salesInten"
-        case salesChangesRatioWithPreviousDay = "salesChange"
-        case salesAccumulation = "salesAcc"
-        case audienceCount = "audiCnt"
-        case audienceCountChangesWithPreviousDay = "audiInten"
-        case audienceCountChangesRatioWithPreviousDay = "audiChange"
-        case audienceAccumulation = "audiAcc"
-        case screenCount = "scrnCnt"
-        case showingCount = "showCnt"
-    }
 }
 
 extension MovieBoxOfficeDTO {
     func toMovieBoxOffice() -> MovieBoxOffice {
         return MovieBoxOffice(from: self)
     }
-}
-
-extension MovieBoxOfficeDTO: Equatable {
-    
 }
