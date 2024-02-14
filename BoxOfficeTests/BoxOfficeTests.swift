380b3ab429034670b98fe6f971d15e8bd74a5c1f
eunjuchoi <eunjuzzoo@naver.com>
2024-02-14 14:28:42 +0900

    feat: BoxOffice 유닛 테스트 파일 기본 설정

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -6,30 +6,24 @@
 //
 
 import XCTest
+@testable import BoxOffice
 
-final class BoxOfficeTests: XCTestCase {
+final class BoxOfficeTests: BaseTestCase {
+    
+    var sut: BoxOfficeData?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // TODO: 객체 생성으로 수정
+        sut = nil
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func test_mock데이터가_모델에_잘_파싱이_되는지() {
+        
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
