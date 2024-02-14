ecc306e5119eee625aa539e44dca4b0294ea6629
suojae3 <suojae3@gmail.com>
2024-02-14 11:42:45 +0900

    feat: UnitTest 추가

---
+++ BoxOfficeUnitTests/BoxOfficeUnitTests.swift
@@ -0,0 +1,44 @@
+//
+//  BoxOfficeUnitTests.swift
+//  BoxOfficeUnitTests
+//
+//  Created by ㅣ on 2/14/24.
+//
+
+import XCTest
+@testable import BoxOffice
+
+final class BoxOfficeUnitTests: XCTestCase {
+    
+    private var sut: BoxOfficeData!
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func test_박스오피스_데이터는_10개씩_가져온다() throws {
+        
+        // given
+        
+        // when
+        
+        // then
+        
+
+    }
+
+    func test_영화_관련_정보의_개수는_X개이다() throws {
+        
+        // given
+        
+        // when
+        
+        // then
+        
+    }
+
+}
