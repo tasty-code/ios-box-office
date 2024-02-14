f8c0f36537213bd380979d30d0ae7b42bda94da6
mireu <148876644+mireu930@users.noreply.github.com>
2024-02-14 23:42:11 +0900

    refactor: Unit Test 적용수정

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -12,15 +12,16 @@ final class BoxOfficeTests: XCTestCase {
 
     func test_인스턴스를통한_Movie타입과Json데이터를_비교했을때_서로같다() {
         // given
-        let dailyBoxOfficeList = DailyBoxOfficeList(rnum: "1", rank: "1", rankInten: "0", rankOldAndNew: "OLD", movieCD: "20236180", movieNm: "웡카", openDt: "2024-01-31", salesAmt: "5175162790", salesShare: "32.8", salesInten: "-132680667", salesChange: "-2.5", salesAcc: "15399911261", audiCnt: "522178", audiInten: "-4556", audiChange: "-0.9", audiAcc: "1585129", scrnCnt: "1440", showCnt: "15062")
+        let dailyBoxOfficeList = DailyBoxOfficeList(rnum: "1", rank: "1", rankInten: "0", rankOldAndNew: RankOldAndNew(rawValue: "NEW") ?? .new, movieCD: "20199882", movieNm: "경관의 피", openDt: "2022-01-05", salesAmt: "584559330", salesShare: "34.2", salesInten: "584559330", salesChange: "100", salesAcc: "631402330", audiCnt: "64050", audiInten: "64050", audiChange: "100", audiAcc: "69228", scrnCnt: "1171", showCnt: "4416")
         
-        let boxOfficeResult = BoxOfficeResult(boxofficeType: "주말 박스오피스", showRange: "20240209~20240211", yearWeekTime: "202406", weeklyBoxOfficeList: [dailyBoxOfficeList])
-        let movie = Movie(boxOfficeResult: boxOfficeResult)
+        let boxOfficeResult = 
+        BoxOfficeResult(boxofficeType: "일별 박스오피스", showRange: "20220105~20220105", dailyBoxOfficeList: [dailyBoxOfficeList])
+        let movie = BoxOfficeData(boxOfficeResult: boxOfficeResult)
         
-        guard let data = Data.jsonData.data(using: .utf8) else { return }
+        guard let data = JsonData.josnData.data(using: .utf8) else { return }
         //when
         do {
-            let jsonData = try JSONDecoder().decode(Movie.self, from: data)
+            let jsonData = try JSONDecoder().decode(BoxOfficeData.self, from: data)
         //then
             XCTAssertEqual(movie, jsonData)
         } catch {
