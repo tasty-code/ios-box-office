d6af27e9d1d76dfc90d25f2a61d50dc2615f05ab
suojae3 <suojae3@gmail.com>
2024-02-14 13:34:51 +0900

    refactor: JsoDecoder 리팩토링

--- BoxOffice/JsonDecoder.swift
+++ BoxOffice/JsonDecoder.swift
@@ -2,12 +2,13 @@
 import Foundation
 
 struct JsonDecoder<Element: Codable> {
+    
     func loadData(from fileName: String, of fileType: String) -> Element? {
         
-        guard let filePath = try? loadFilePath(from: fileName, of: fileType) else { return nil }
-        guard let data = try? loadFileData(from: filePath) else { return nil }
-        guard let jsonData = try? decodeFileData(from: data) else { return nil }
-    
+        guard let filePath = try? loadFilePath(from: fileName, of: fileType),
+              let data = try? loadFileData(from: filePath),
+              let jsonData = try? decodeFileData(from: data) else { return nil }
+
         return jsonData
     }
     
@@ -15,16 +16,17 @@ struct JsonDecoder<Element: Codable> {
         guard let filePath = Bundle.main.path(forResource: fileName, ofType: fileType) else {
             print("\(JsonParsingError.pathError.errorMessage)")
             throw JsonParsingError.pathError }
-        
         return filePath
     }
     
-    private func loadFileData(from filePath: String) throws -> Data? {
-        guard let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)) else { throw JsonParsingError.dataLoadError }
+    private func loadFileData(from filePath: String) throws -> Data {
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)) else {
+            print("\(JsonParsingError.dataLoadError.errorMessage)")
+            throw JsonParsingError.dataLoadError }
         return data
     }
     
-    private func decodeFileData(from fileData: Data) throws -> Element? {
+    private func decodeFileData(from fileData: Data) throws -> Element {
         guard let jsonData = try? JSONDecoder().decode(Element.self, from: fileData) else {
             print("\(JsonParsingError.decodingError.errorMessage)")
             throw JsonParsingError.decodingError }
