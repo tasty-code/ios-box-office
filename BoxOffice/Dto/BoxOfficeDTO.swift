7fa30e7ced9796db4b0b44de618e5ad83a34f51a
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-02-14 15:21:46 +0900

    feat: BoxofficeCodingKeys enum 생성

--- BoxOffice/Dto/BoxOfficeDTO.swift
+++ BoxOffice/Dto/BoxOfficeDTO.swift
@@ -1,13 +1,19 @@
 import Foundation
 
 struct BoxOfficeDTO: Decodable {
-    private let boxofficeType: String
+    private let boxOfficeType: String
     private let showRange: String
     private let movieBoxOfficeList: Array<MovieBoxOfficeDTO>
     
-    init(boxofficeType: String, showRange: String, movieBoxOfficeList: Array<MovieBoxOfficeDTO>) {
-        self.boxofficeType = boxofficeType
+    init(boxOfficeType: String, showRange: String, movieBoxOfficeList: Array<MovieBoxOfficeDTO>) {
+        self.boxOfficeType = boxOfficeType
         self.showRange = showRange
         self.movieBoxOfficeList = movieBoxOfficeList
     }
+}
+
+enum BoxofficeCodingKeys: String, CodingKey {
+    case boxOfficeType = "boxofficeType"
+    case showRange = "showRange"
+    case movieBoxOfficeList = "dailyBoxOfficeList"
 }
