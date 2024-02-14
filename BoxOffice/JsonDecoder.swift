e4a9f20bd3bac2095eacdc56b8b7c8f588ce2652
hemil0102 <hemil0102@gmail.com>
2024-02-14 13:20:50 +0900

    feat: Json 디코딩을 위한JsonDecoder() 타입 구현

---
+++ BoxOffice/JsonDecoder.swift
@@ -0,0 +1,33 @@
+
+import Foundation
+
+struct JsonDecoder<Element: Codable> {
+    func loadData(from fileName: String, of fileType: String) -> Element? {
+        
+        guard let filePath = try? loadFilePath(from: fileName, of: fileType) else { return nil }
+        guard let data = try? loadFileData(from: filePath) else { return nil }
+        guard let jsonData = try? decodeFileData(from: data) else { return nil }
+    
+        return jsonData
+    }
+    
+    private func loadFilePath(from fileName: String, of fileType: String) throws -> String {
+        guard let filePath = Bundle.main.path(forResource: fileName, ofType: fileType) else {
+            print("\(JsonParsingError.pathError.errorMessage)")
+            throw JsonParsingError.pathError }
+        
+        return filePath
+    }
+    
+    private func loadFileData(from filePath: String) throws -> Data? {
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)) else { throw JsonParsingError.dataLoadError }
+        return data
+    }
+    
+    private func decodeFileData(from fileData: Data) throws -> Element? {
+        guard let jsonData = try? JSONDecoder().decode(Element.self, from: fileData) else {
+            print("\(JsonParsingError.decodingError.errorMessage)")
+            throw JsonParsingError.decodingError }
+        return jsonData
+    }
+}
