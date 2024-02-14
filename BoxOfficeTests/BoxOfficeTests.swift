be83fa1c706267912c573c27130f0659e630463c
angryeon7 <nayeon7612@gmail.com>
2024-02-14 16:37:41 +0900

    feat: 데이터모델 테스트코드 작성

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -10,20 +10,55 @@ import XCTest
 
 final class BoxOfficeTests: BaseTestCase {
     
-    var sut: BoxOfficeData?
-
-    override func setUpWithError() throws {
-        // TODO: 객체 생성으로 수정
-        sut = nil
-    }
-
-    override func tearDownWithError() throws {
-        sut = nil
-    }
+    var sut: BoxOfficeData!
+    var mockData: NSDataAsset!
     
-    func test_mock데이터가_모델에_잘_파싱이_되는지() {
+    override func setUpWithError() throws {
         
     }
-
-
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        mockData = nil
+    }
+    
+    func test_mock데이터가_파싱되어_nil값을_반환하지_않는지() {
+        
+        given {
+            guard let json = NSDataAsset(name: "MockData", bundle: Bundle(for: BoxOfficeTests.self)) else {
+                return
+            }
+            mockData = json
+        }
+        
+        when {
+            sut = try? JSONDecoder().decode(BoxOfficeData.self, from: mockData.data)
+        }
+        
+        then {
+            XCTAssertNotNil(sut)
+        }
+    }
+    
+    func test_mock데이터가_Parsing_후_dailyBoxOfficeList안에_다섯번째_요소의_movieName이_해피_뉴_이어_반환() {
+        var result: String!
+        var expectation: String?
+        
+        given {
+            guard let dataAsset = NSDataAsset(name: "MockData", bundle: Bundle(for: BoxOfficeTests.self)) else {
+                return
+            }
+            mockData = dataAsset
+            expectation = "해피 뉴 이어"
+        }
+        
+        when {
+            sut = try? JSONDecoder().decode(BoxOfficeData.self, from: mockData.data)
+            result = sut.boxOfficeResult.dailyBoxOfficeList[4].movieName
+        }
+        
+        then {
+            XCTAssertEqual(result, expectation)
+        }
+    }
 }
