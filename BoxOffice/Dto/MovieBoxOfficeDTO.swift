cd886dd438fa29fd2d5cd3760890506fba2daef5
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-02-14 15:14:07 +0900

    feat: MovieBoxOfficeDTO 구조체 생성

--- BoxOffice/Dto/MovieBoxOfficeDTO.swift
+++ BoxOffice/Dto/MovieBoxOfficeDTO.swift
@@ -1,5 +1,21 @@
 import Foundation
 
 struct MovieBoxOfficeDTO: Decodable {
-    
+    private let rank: String
+    private let rankChangesWithPreviousDay: String
+    private let rankOldAndNew: String
+    private let movieCode: String
+    private let movieName: String
+    private let openingDate: String
+    private let salesAmount: String
+    private let salesShare: String
+    private let salesChangesWithPreviousDay: String
+    private let salesChangesRatioWithPreviousDay: String
+    private let salesAccumulation: String
+    private let audienceCount: String
+    private let audienceCountChangesWithPreviousDay: String
+    private let audienceCountChangesRatioWithPreviousDay: String
+    private let audienceAccumulation: String
+    private let screenCount: String
+    private let showingCount: String
 }
