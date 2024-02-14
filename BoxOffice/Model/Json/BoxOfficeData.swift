365b59940a6f982c2e18535e99bbbe2a54ab8d37
wlsdud-dev <hjymail1025@gmail.com>
2024-02-14 13:59:35 +0900

    feat: Booxofiice 데이터 모델 구현

---
+++ BoxOffice/Model/Json/BoxOfficeData.swift
@@ -0,0 +1,63 @@
+//
+//  JsonPasing.swift
+//  BoxOffice
+//
+//  Created by Jin-Mac on 2/14/24.
+//
+
+import Foundation
+
+enum RankOldAndNew: String, Codable {
+    case new = "NEW"
+    case old = "OLD"
+}
+
+struct BoxOfficeData: Codable {
+    let boxOfficeResult: BoxOfficeResult
+}
+
+struct BoxOfficeResult: Codable {
+    let boxofficeType: String
+    let showRange: String
+    let dailyBoxOfficeList: [DailyBoxOfficeList]
+}
+
+struct DailyBoxOfficeList: Codable {
+    let number: String
+    let rank: String
+    let rankIncrement: String
+    let rankOldAndNew: RankOldAndNew
+    let movieCd: String
+    let movieName: String
+    let openDate: String
+    let salesAmount: String
+    let salesShare: String
+    let salesIncrement: String
+    let salesChange: String
+    let salesAccount: String
+    let audienceCount: String
+    let audienceIncrement: String
+    let audienceChange: String
+    let audienceAccount: String
+    let screenCount: String
+    let showCount: String
+    
+    enum CodingKeys: String, CodingKey{
+        case number = "rnum"
+        case rankIncrement = "rankInten"
+        case movieName = "movieNm"
+        case openDate = "openDt"
+        case salesAmount = "salesAmt"
+        case salesShare = "salesShare"
+        case salesIncrement = "salesInten"
+        case salesChange = "salesChange"
+        case salesAccount = "salesAcc"
+        case audienceCount = "audiCnt"
+        case audienceIncrement = "audiInten"
+        case audienceChange = "audiChange"
+        case audienceAccount = "audiAcc"
+        case screenCount = "scrnCnt"
+        case showCount = "showCnt"
+        case rankOldAndNew, rank, movieCd
+    }
+}
