9b3a7e2f64cf7efcd28e7b5fc41991566bdb78a7
mireu <148876644+mireu930@users.noreply.github.com>
2024-02-15 23:34:03 +0900

    refactor: Unit테스트 수정

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -10,20 +10,16 @@ import XCTest
 
 final class BoxOfficeTests: XCTestCase {
 
-    func test_인스턴스를통한_Movie타입과Json데이터를_비교했을때_서로같다() {
+    func test_BoxOfficeDataResponse타입이Decodable기능을_잘수행해서_JSON데이터를_파싱하는데성공한다면_nil이아니다() {
         // given
-        let dailyBoxOfficeList = DailyBoxOfficeInfo(number: "1", rank: "1", rankFluctuation: "0", rankOldAndNew: "NEW", movieCode: "20199882", movieName: "경관의 피", openDate: "2022-01-05", salesAmount: "584559330", salesShare: "34.2", salesFluctuation: "584559330", salesChange: "100", salesAccumulation: "631402330", audienceCount: "64050", audienceFluctuation: "64050", audienceChange: "100", audienceAccumulation: "69228", screenCount: "1171", showCount: "4416")
-        
-        let boxOfficeResult = 
-        BoxOfficeResult(boxOfficeType: "일별 박스오피스", showRange: "20220105~20220105", dailyBoxOfficeList: [dailyBoxOfficeList])
-        let movie = BoxOfficeDataResponse(boxOfficeResult: boxOfficeResult)
+
         
         guard let data = JsonData.josnData.data(using: .utf8) else { return }
         //when
         do {
             let jsonData = try JSONDecoder().decode(BoxOfficeDataResponse.self, from: data)
         //then
-            XCTAssertEqual(movie, jsonData)
+            XCTAssertNotNil(jsonData)
         } catch {
             XCTFail()
         }
