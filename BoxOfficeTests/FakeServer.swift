424111a93e974a2dab8fbe3b44dedacb1e3fc978
eensungkim <eensungkim@gmail.com>
2024-02-15 21:22:03 +0900

    fix: 랜덤한 서버에러 구현부 삭제

--- BoxOfficeTests/FakeServer.swift
+++ BoxOfficeTests/FakeServer.swift
@@ -28,10 +28,6 @@ class FakeServer {
                 throw HTTPStatusError.clientError
             }
             
-            if Int.random(in: 1...10) == 1 {
-                throw HTTPStatusError.serverError
-            }
-            
             guard let input = """
             {"boxOfficeResult":{"boxofficeType":"일별 박스오피스","showRange":"20220105~20220105","dailyBoxOfficeList":[{"rnum":"1","rank":"1","rankInten":"0","rankOldAndNew":"NEW","movieCd":"20199882","movieNm":"경관의 피","openDt":"2022-01-05","salesAmt":"584559330","salesShare":"34.2","salesInten":"584559330","salesChange":"100","salesAcc":"631402330","audiCnt":"64050","audiInten":"64050","audiChange":"100","audiAcc":"69228","scrnCnt":"1171","showCnt":"4416"}]}}
             """.data(using: .utf8) else { throw HTTPStatusError.serverError }
@@ -40,10 +36,6 @@ class FakeServer {
         case .movieDetailInfomation("", ""):
             guard fakeURL_MovieDetailInfomation == url.absoluteString else {
                 throw HTTPStatusError.clientError
-            }
-            
-            if Int.random(in: 1...10) == 1 {
-                throw HTTPStatusError.serverError
             }
             
             guard let input = """
