755dd19100add1db0f66d64fdcf72d33ce910541
eensungkim <eensungkim@gmail.com>
2024-02-15 16:03:00 +0900

    feat: 네트워크 요청 메서드 추가

--- BoxOffice/Model/BoxOfficeDTO.swift
+++ BoxOffice/Model/BoxOfficeDTO.swift
@@ -17,18 +17,37 @@ final class BoxOfficeDTO {
 }
 
 extension BoxOfficeDTO {
+    func requestData(with url: String) async throws -> Data? {
+        guard let URL = URL(string: url) else { 
+            return nil
+        }
+        
+        let data = try await URLSession.shared.data(from: URL)
+        
+        guard let httpResponse = data.1 as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else { 
+            return nil
+        }
+        
+        return data.0
+    }
+    
     func parseJSONData(_ data: Data? = nil) -> BoxOfficeDetail? {
         
         guard let fileLocation = Bundle.main.url(forResource: "box_office_sample", withExtension: "json") else {
             return nil
         }
+        
         do {
             guard let jsonData = data == nil ? try Data(contentsOf: fileLocation) : data else {
                 return nil
             }
+            
             let decoder = JSONDecoder()
             let data = try decoder.decode(BoxOfficeResult.self, from: jsonData)
+            
             return data.boxOfficeResult
+            
         } catch {
             print(error)
             return nil
