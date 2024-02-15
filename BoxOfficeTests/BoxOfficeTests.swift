4d7b89a3c754d83ad5744539ba3802bb566173df
Matthew(매튜) <krsder@naver.com>
2024-02-15 16:04:27 +0900

    test: testurl 타입 변경

--- BoxOfficeTests/BoxOfficeTests.swift
+++ BoxOfficeTests/BoxOfficeTests.swift
@@ -13,12 +13,12 @@ final class BoxOfficeTests: XCTestCase {
     func test_date가_20230101이고_데이터_파싱이_올바르게_됐을_때_fetchData는_nil이_아니다() {
         // given
         let date = "20170319"
-        let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=\(date)"
+        var urlString = MovieURL.makeURL(date: date)
         
         // when
         let expectation = XCTestExpectation(description: "데이터 패치 중...")
         
-        APIService.fetchData(urlString: url) { (result: Result<Movie, NetworkError>) in
+        APIService.fetchData(urlString: urlString) { (result: Result<Movie, NetworkError>) in
             switch result {
             case .success(let movies):
                 // then
@@ -36,12 +36,12 @@ final class BoxOfficeTests: XCTestCase {
     func test_date가_잘못된_타입으로_데이터_파싱_됐을_때_fetchMovie에서_decodingError발생() {
         // given
         let wrongDate = "iWantToGoHome"
-        let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=\(wrongDate)"
+        let urlString = MovieURL.makeURL(date: wrongDate)
         
         // when
         let expectation = XCTestExpectation(description: "데이터 패치 중...")
         
-        APIService.fetchData(urlString: url) { (result: Result<Movie, NetworkError>) in
+        APIService.fetchData(urlString: urlString) { (result: Result<Movie, NetworkError>) in
             switch result {
             case .success(let movies):
                 // then
