936b48846124203c94541aec867967fd7175b1f6
Matthew(매튜) <krsder@naver.com>
2024-02-15 16:03:33 +0900

    feat: Bundle apiKey Extension 추가

--- BoxOffice/Extension/Bundle+Extension.swift
+++ BoxOffice/Extension/Bundle+Extension.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Bundle {
+    
+    var apiKey: String {
+        guard 
+            let file = self.path(forResource: "Private", ofType: "plist"),
+            let resource = NSDictionary(contentsOfFile: file),
+            let key = resource["API_KEY"] as? String
+        else {
+            return ""
+        }
+        return key
+    }
+}
--- BoxOffice/Model/MovieURL.swift
+++ BoxOffice/Model/MovieURL.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
-struct URL {
-    var date: String
-    private var key = Bundle.main.apiKey
-    var url: String
+struct MovieURL {
+    
+    static func makeURL(date: String) -> String {
+        let key = Bundle.main.apiKey
+        var url: String {
+            return "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(String(describing: key))&targetDt=\(date)"
+        }
+        return url
+    }
 }
