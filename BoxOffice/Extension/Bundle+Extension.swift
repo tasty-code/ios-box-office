25d8e9abd7b71d8b907755c3dcc1cac30a958dd9
Matthew(매튜) <krsder@naver.com>
2024-02-15 16:01:36 +0900

    feat: plist API_KEY 생성

---
+++ BoxOffice/Extension/Bundle+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Bundel+Extension.swift
+//  BoxOffice
+//
+//  Created by Matthew on 2/15/24.
+//
+
+import Foundation
---
+++ BoxOffice/Model/MovieURL.swift
@@ -0,0 +1,14 @@
+//
+//  URL.swift
+//  BoxOffice
+//
+//  Created by Matthew on 2/15/24.
+//
+
+import Foundation
+
+struct URL {
+    var date: String
+    private var key = Bundle.main.apiKey
+    var url: String
+}
