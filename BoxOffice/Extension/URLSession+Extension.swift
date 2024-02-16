0855723c464972c841da0a3f6e2977c52f2b44d1
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-16 18:05:16 +0900

    feat: URLSession 및 URLSessionDataTask 프로토콜 채택

--- BoxOffice/Extension/URLSession+Extension.swift
+++ BoxOffice/Extension/URLSession+Extension.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension URLSession: URLSessionProtocol {
+    func dataTask(
+        with url: URL,
+        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTaskProtocol {
+        return dataTask(
+            with: url,
+            completionHandler: completionHandler
+        ) as URLSessionDataTask
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
