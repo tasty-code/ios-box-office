f12bc92c735105b9de95d03bbe87db9239768e56
LeeSe0ngYe0n <goblin__@naver.com>
2024-02-16 16:48:27 +0900

    refactor: BoxOfficeDto 변경에 따라 fake도 수정

--- BoxOfficeTests/Utility/FakeBoxOfficeDTO.swift
+++ BoxOfficeTests/Utility/FakeBoxOfficeDTO.swift
@@ -6,20 +6,40 @@
 //
 
 import Foundation
+@testable import BoxOffice
 
 struct FakeBoxOfficeDTO {
-    static func requestData(with url: String) throws -> Data? {
-        guard let URL = URL(string: url) else {
+    static func requestData(with url: String) async -> Data? {
+        do {
+            guard let URL = URL(string: url) else {
+                return nil
+            }
+            
+            let data = try FakeServer.data(from: URL)
+            
+            guard let httpResponse = data.1 else {
+                return nil
+            }
+            
+            switch httpResponse.statusCode {
+            case 200...299:
+                return data.0
+            case 300...399:
+                throw HTTPStatusError.redirection
+            case 400...499:
+                throw HTTPStatusError.clientError
+            case 500...599:
+                throw HTTPStatusError.serverError
+            default:
+                return nil
+            }
+            
+        } catch {
+            print(error)
             return nil
         }
-        
-        let data = try FakeServer.data(from: URL)
-        
-        guard let httpResponse = data.1,
-              (200...299).contains(httpResponse.statusCode) else {
-            return nil
-        }
-        
-        return data.0
     }
+
 }
+
+
