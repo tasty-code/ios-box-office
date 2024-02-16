c76c34a1526b39eccc283597f79ce6252d0beb19
eensungkim <eensungkim@gmail.com>
2024-02-16 16:55:04 +0900

    feat: 데이터 확인을 위한 임시 코드 추가

--- BoxOffice/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Controller/BoxOfficeViewController.swift
@@ -13,6 +13,14 @@ final class BoxOfficeViewController: UIViewController {
     
     init() {
         super.init(nibName: nil, bundle: nil)
+        Task {
+            let apiKey: String = Bundle.main.apiKey
+            guard let jsonData = await BoxOfficeDTO.requestData(with: KoreanFilmCouncilURLEnumeration.dailyBoxOffice(apiKey, "20240215").url) else {
+                return
+            }
+            dataSource = BoxOfficeDTO.parseJSONData(jsonData)
+            print(dataSource)
+        }
     }
     
     required init?(coder: NSCoder) {
