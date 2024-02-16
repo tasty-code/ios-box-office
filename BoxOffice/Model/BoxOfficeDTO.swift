a79a0f16b21e63098418ef8e26bd4500762566c4
LeeSe0ngYe0n <goblin__@naver.com>
2024-02-16 16:39:37 +0900

    refactor: requestData 메서드 내에서 에러를 핸들링 하도록 변경

--- BoxOffice/Model/BoxOfficeDTO.swift
+++ BoxOffice/Model/BoxOfficeDTO.swift
@@ -8,27 +8,33 @@
 import Foundation
 
 struct BoxOfficeDTO {
-    static func requestData(with url: String) async throws -> Data? {
-        guard let URL = URL(string: url) else {
-            return nil
-        }
-        
-        let data = try await URLSession.shared.data(from: URL)
-        
-        guard let httpResponse = data.1 as? HTTPURLResponse else {
-            return nil
-        }
-        
-        switch httpResponse.statusCode {
-        case 200...299:
-            return data.0
-        case 300...399:
-            throw HTTPStatusError.redirection
-        case 400...499:
-            throw HTTPStatusError.clientError
-        case 500...599:
-            throw HTTPStatusError.serverError
-        default:
+    static func requestData(with url: String) async -> Data? {
+        do {
+            guard let URL = URL(string: url) else {
+                return nil
+            }
+            
+            let data = try await URLSession.shared.data(from: URL)
+            
+            guard let httpResponse = data.1 as? HTTPURLResponse else {
+                return nil
+            }
+            
+            switch httpResponse.statusCode {
+            case 200...299:
+                return data.0
+            case 300...399:
+                throw HTTPStatusError.redirection
+            case 400...499:
+                throw HTTPStatusError.clientError
+            case 500...599:
+                throw HTTPStatusError.serverError
+            default:
+                return nil
+            }
+            
+        } catch {
+            print(error)
             return nil
         }
     }
