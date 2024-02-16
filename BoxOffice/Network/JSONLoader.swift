ce8146a5548ed84eff0bcf0826c8566ad4f097b5
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-16 18:05:47 +0900

    feat: JSON 데이터 파싱 로직 구현

--- BoxOffice/Network/JSONLoader.swift
+++ BoxOffice/Network/JSONLoader.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct JSONLoader {
+    static func load() -> Data? {
+        let fileName: String = "box_office"
+        let extensionType = "json"
+        
+        guard
+            let fileLocation = Bundle.main.url(forResource: fileName, withExtension: extensionType)
+        else {
+            print("Failed to get file location for \(fileName).json")
+            return nil
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            return data
+        } catch {
+            print("Failed to load data from \(fileLocation.path): \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
