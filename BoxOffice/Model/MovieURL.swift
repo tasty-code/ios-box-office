7d6ee3fe53ab274372baf9134d31b6e694c4fd9f
Matthew(매튜) <krsder@naver.com>
2024-02-16 14:23:36 +0900

    refactor: 오늘날짜 기준 -> 어제날짜 기준 으로 명세서 반영

--- BoxOffice/Model/MovieURL.swift
+++ BoxOffice/Model/MovieURL.swift
@@ -16,11 +16,15 @@ struct MovieURL {
         return url
     }
     
-    static func makeTodayBoxOfficeURL() -> String {
+    static func makeYesterdayBoxOfficeURL() -> String {
         let key = Bundle.main.apiKey
-        let today = Date()
+        guard 
+            let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+        else {
+            return dateFormatter(date: Date() - 86400)
+        }
         var url: String {
-            return "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(String(describing: key))&targetDt=\(dateFormatter(date: today)))"
+            return "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(String(describing: key))&targetDt=\(dateFormatter(date: yesterday))"
         }
         return url
     }
@@ -28,7 +32,7 @@ struct MovieURL {
     static func makeMovieDetailURL(code: String) -> String {
         let key = Bundle.main.apiKey
         var url: String {
-            return "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(String(describing: key))&movieCd=\(code))"
+            return "https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=\(String(describing: key))&movieCd=\(code)"
         }
         return url
     }
