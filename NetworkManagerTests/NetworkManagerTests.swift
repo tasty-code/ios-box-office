4c8d606665a63ce70e4eb7cbd42de5431b5fe526
mireu <148876644+mireu930@users.noreply.github.com>
2024-02-17 01:16:55 +0900

    docs: NetworkManager UnitTest 파일생성

---
+++ NetworkManagerTests/NetworkManagerTests.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkManagerTests.swift
+//  NetworkManagerTests
+//
+//  Created by Lee minyeol on 2/17/24.
+//
+
+import XCTest
+
+final class NetworkManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
