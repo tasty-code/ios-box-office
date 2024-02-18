90d85d71c2eac0ac6028dbabe546808daff5d6a2
Effie <effie.thedev@gmail.com>
2024-02-19 01:22:11 +0900

    feat: NetworkService 구현

---
+++ BoxOffice/NetworkService.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+enum NetworkServiceError: Error {
+  case urlError(error: URLError)
+  case invalidHTTPResponse
+  case noSuccess
+}
+
+protocol URLRequestConvertible {
+  func toURLRequest() -> URLRequest
+}
+
+struct NetworkRequest {
+  
+}
+
+//extension NetworkRequest: URLRequestConvertible {
+//  func toURLRequest() -> URLRequest {
+//    return .init(url: .init(string: "") )
+//  }
+//}
+
+protocol NetworkRequestable {
+  func requestData(request: URLRequestConvertible) async -> Result<Data, NetworkServiceError>
+}
+
+struct NetworkService {
+  private let session: URLSession
+  
+  init(session: URLSession) {
+    self.session = session
+  }
+}
+
+extension NetworkService: NetworkRequestable {
+  func requestData(request: URLRequestConvertible) async -> Result<Data, NetworkServiceError> {
+    do {
+      let (data, response) = try await self.session.data(for: request.toURLRequest())
+      guard 
+        let httpResponse = response as? HTTPURLResponse,
+        httpResponse.statusCode == 200
+      else {
+        return .failure(.noSuccess)
+      }
+      return .success(data)
+    } catch {
+      let finalError = convertErrorToNetworkServiceError(error)
+      return .failure(finalError)
+    }
+  }
+  
+  private func convertErrorToNetworkServiceError(_ error: Error) -> NetworkServiceError {
+    if let networkError = error as? NetworkServiceError {
+      return networkError
+    } else if let urlError = error as? URLError {
+      return .urlError(error: urlError)
+    } else {
+      return .invalidHTTPResponse
+    }
+  }
+}
