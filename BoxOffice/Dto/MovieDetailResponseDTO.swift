05cdfe4d0eaee2c0da6ea82872380fe10c8450bd
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-02-19 10:16:53 +0900

    feat: 누락된 MovieDetailResponseDTO 복구

---
+++ BoxOffice/Dto/MovieDetailResponseDTO.swift
@@ -0,0 +1,171 @@
+import Foundation
+
+struct MovieDetailResponseDTO: Decodable {
+    let movieInfoResult: MovieInfoResultDTO
+}
+
+extension MovieDetailResponseDTO {
+    struct MovieInfoResultDTO: Decodable {
+        let movieInfo: MovieInfoDTO
+        let source: String
+    }
+}
+
+extension MovieDetailResponseDTO.MovieInfoResultDTO {
+    struct MovieInfoDTO: Decodable {
+        let movieCode: String
+        let movieName: String
+        let englishMovieName: String
+        let originalMovieName: String
+        let productionYear: String
+        let duration: String
+        let openingDate: String
+        let productionStatus: String
+        let typeNm: String
+        let nations: [Nation]
+        let genres: [Genre]
+        let directors: [Director]
+        let actors: [Actor]
+        let showTypes: [ShowType]
+        let companies: [Company]
+        let audits: [Audit]
+        let staffs: [Staff]
+    }
+}
+extension MovieDetailResponseDTO.MovieInfoResultDTO.MovieInfoDTO {
+    enum CodingKeys: String, CodingKey {
+        case movieCode = "movieCd"
+        case movieName = "movieNm"
+        case englishMovieName = "movieNmEn"
+        case originalMovieName = "movieNmOg"
+        case productionYear = "prdtYear"
+        case duration = "showTm"
+        case openingDate = "openDt"
+        case productionStatus = "prdtStatNm"
+        case typeNm
+        case nations
+        case genres
+        case directors
+        case actors
+        case showTypes
+        case companies = "companys"
+        case audits
+        case staffs
+    }
+}
+
+extension MovieDetailResponseDTO.MovieInfoResultDTO.MovieInfoDTO {
+    func toModel() -> MovieInfoList? {
+        guard let movieCode = Int(movieCode),
+              let productionYear = Int(productionYear),
+              let duration = Int(duration),
+              let openingDate = openingDate.toDate() else {
+            return nil
+        }
+        return MovieInfoList(
+            movieCode: movieCode,
+            movieName: movieName,
+            englishMovieName: englishMovieName,
+            originalMovieName: originalMovieName,
+            productionYear: productionYear,
+            duration: duration,
+            openingDate: openingDate,
+            productionStatus: productionStatus,
+            typeNm: typeNm,
+            nations: nations,
+            genres: genres,
+            directors: directors,
+            actors: actors,
+            showTypes: showTypes,
+            companies: companies,
+            audits: audits,
+            staffs: staffs
+        )
+    }
+}
+
+struct Nation: Decodable {
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "nationNm"
+    }
+}
+
+struct Genre: Decodable {
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "genreNm"
+    }
+}
+
+struct Director: Decodable {
+    let name: String
+    let englishName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "peopleNm"
+        case englishName = "peopleNmEn"
+    }
+}
+
+struct Actor: Decodable {
+    let name: String
+    let englishName: String
+    let role: String
+    let castEnglish: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "peopleNm"
+        case englishName = "peopleNmEn"
+        case role = "cast"
+        case castEnglish = "castEn"
+    }
+}
+
+struct ShowType: Decodable {
+    let groupName: String
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case groupName = "showTypeGroupNm"
+        case name = "showTypeNm"
+    }
+}
+
+struct Company: Decodable {
+    let code: String
+    let name: String
+    let englishName: String
+    let partName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case code = "companyCd"
+        case name = "companyNm"
+        case englishName = "companyNmEn"
+        case partName = "companyPartNm"
+    }
+}
+
+struct Audit: Decodable {
+    let auditNumber: String
+    let rating: String
+    
+    enum CodingKeys: String, CodingKey {
+        case auditNumber = "auditNo"
+        case rating = "watchGradeNm"
+    }
+}
+
+struct Staff: Decodable {
+    let name: String
+    let englishName: String
+    let role: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "peopleNm"
+        case englishName = "peopleNmEn"
+        case role = "staffRoleNm"
+    }
+}
