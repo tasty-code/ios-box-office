79c2ab608c51b89805b8a4d2a6076790938920b2
eensungkim <eensungkim@gmail.com>
2024-02-19 21:16:07 +0900

    feat: 네트워크 요청을 생성하는 기능 추가(makeRequest)

--- BoxOffice/Utility/NetworkManager.swift
+++ BoxOffice/Utility/NetworkManager.swift
@@ -9,12 +9,20 @@ import Foundation
 
 struct NetworkManager {
     private(set) var urlSession: URLSession?
+    private(set) var apiKey: String = Bundle.main.apiKey
     
     init(urlSession: URLSession) {
         self.urlSession = urlSession
     }
     
-    func requestData<T>(with url: String, to type: T.Type) async -> T? where T : Decodable {
+    func makeRequest<T: Decodable>(_ type: T.Type, for queryValue: String, with url: KoreanFilmCouncilURL) async -> T? {
+        guard let data = await requestData(to: KoreanFilmCouncilURL.dailyBoxOffice(key: apiKey, queryValue: queryValue).url, into: type.self) else {
+            return nil
+        }
+        return data
+    }
+    
+    func requestData<T>(to url: String, into type: T.Type) async -> T? where T : Decodable {
         do {
             guard let URL = URL(string: url) else {
                 return nil
