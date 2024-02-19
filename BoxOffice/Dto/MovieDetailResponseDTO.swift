0595ee13de252a1141b0c370767192c534926a07
jus1234 <osjamg1996@gmail.com>
2024-02-19 11:44:51 +0900

    refactor: Movie 모델 리팩토링

--- BoxOffice/Dto/MovieDetailResponseDTO.swift
+++ BoxOffice/Dto/MovieDetailResponseDTO.swift
@@ -21,7 +21,7 @@ extension MovieDetailResponseDTO.MovieInfoResultDTO {
         let duration: String
         let openingDate: String
         let productionStatus: String
-        let typeNm: String
+        let runtimeType: String
         let nations: [Nation]
         let genres: [Genre]
         let directors: [Director]
@@ -42,7 +42,7 @@ extension MovieDetailResponseDTO.MovieInfoResultDTO.MovieDTO {
         case duration = "showTm"
         case openingDate = "openDt"
         case productionStatus = "prdtStatNm"
-        case typeNm
+        case runtimeType = "typeNm"
         case nations
         case genres
         case directors
@@ -55,117 +55,9 @@ extension MovieDetailResponseDTO.MovieInfoResultDTO.MovieDTO {
 }
 
 extension MovieDetailResponseDTO.MovieInfoResultDTO.MovieDTO {
-    func toModel() -> Movie? {
-        guard let movieCode = Int(movieCode),
-              let productionYear = Int(productionYear),
-              let duration = Int(duration),
-              let openingDate = openingDate.toDate() else {
-            return nil
-        }
-        return Movie(
-            movieCode: movieCode,
-            movieName: movieName,
-            englishMovieName: englishMovieName,
-            originalMovieName: originalMovieName,
-            productionYear: productionYear,
-            duration: duration,
-            openingDate: openingDate,
-            productionStatus: productionStatus,
-            typeNm: typeNm,
-            nations: nations,
-            genres: genres,
-            directors: directors,
-            actors: actors,
-            showTypes: showTypes,
-            companies: companies,
-            audits: audits,
-            staffs: staffs
-        )
+    func toModel() -> Movie {
+        return .init(from: self)
     }
 }
 
-struct Nation: Decodable {
-    let name: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case name = "nationNm"
-    }
-}
 
-struct Genre: Decodable {
-    let name: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case name = "genreNm"
-    }
-}
-
-struct Director: Decodable {
-    let name: String
-    let englishName: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case name = "peopleNm"
-        case englishName = "peopleNmEn"
-    }
-}
-
-struct Actor: Decodable {
-    let name: String
-    let englishName: String
-    let role: String
-    let castEnglish: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case name = "peopleNm"
-        case englishName = "peopleNmEn"
-        case role = "cast"
-        case castEnglish = "castEn"
-    }
-}
-
-struct ShowType: Decodable {
-    let groupName: String
-    let name: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case groupName = "showTypeGroupNm"
-        case name = "showTypeNm"
-    }
-}
-
-struct Company: Decodable {
-    let code: String
-    let name: String
-    let englishName: String
-    let partName: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case code = "companyCd"
-        case name = "companyNm"
-        case englishName = "companyNmEn"
-        case partName = "companyPartNm"
-    }
-}
-
-struct Audit: Decodable {
-    let auditNumber: String
-    let rating: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case auditNumber = "auditNo"
-        case rating = "watchGradeNm"
-    }
-}
-
-struct Staff: Decodable {
-    let name: String
-    let englishName: String
-    let role: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case name = "peopleNm"
-        case englishName = "peopleNmEn"
-        case role = "staffRoleNm"
-    }
-}
