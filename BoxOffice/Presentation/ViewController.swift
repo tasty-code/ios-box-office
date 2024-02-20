e0e74e18efbc06c4e91ad88ea53e769316cc741c
angryeon7 <nayeon7612@gmail.com>
2024-02-20 19:29:15 +0900

    feat: 영화상세정보 조회 기능 구현

--- BoxOffice/Presentation/ViewController.swift
+++ BoxOffice/Presentation/ViewController.swift
@@ -7,9 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else { return }
+        fetchMovieDetailData(userKey: apiKey, movieCode: "20124079")
+    }
+    
+    private func fetchMovieDetailData(userKey: String, movieCode: String) {
+        MovieAPI.shared.requestMovieDetailAPI(userKey: userKey,
+                                              movieCode: movieCode) { networkResult in
+            switch networkResult {
+                
+            case .success(let data):
+                if let data = data as? MovieInformation {
+                    print(data)
+                }
+            case .pathError:
+                print("pathErr")
+            case .requestError(_):
+                print("requestErr")
+            case .networkFail:
+                print("networkFail")
+            case .serverError:
+                print("serverErr")
+                
+            }
+        }
     }
 }
