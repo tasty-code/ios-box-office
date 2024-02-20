6bf08d93ffa3eb3c640f99014a40afe43bb74b5a
eunjuchoi <eunjuzzoo@naver.com>
2024-02-20 17:41:49 +0900

    feat: MovieService 정의

---
+++ BoxOffice/Network/Service/MovieService.swift
@@ -0,0 +1,47 @@
+//
+//  MovieServices.swift
+//  BoxOffice
+//
+//  Created by EUNJU on 2/20/24.
+//
+
+import Foundation
+
+enum MovieService {
+    case requestMovieDetailInfo(userKey: String, movieCode: String)
+}
+
+extension MovieService: TargetType {
+    
+    var method: HTTPMethod {
+        switch self {
+        case .requestMovieDetailInfo(_, _):
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .requestMovieDetailInfo(_, _):
+            return "/movie/searchMovieInfo.json"
+        }
+    }
+    
+    var parameters: RequestParameters {
+        switch self {
+        case .requestMovieDetailInfo(let userKey, let movieCode):
+            let requestQuery: [String: Any] = [
+                "key": userKey,
+                "movieCd": movieCode
+            ]
+            return .query(requestQuery)
+        }
+    }
+    
+    var header: HeaderType {
+        switch self {
+        case .requestMovieDetailInfo(_, _):
+            return .basic
+        }
+    }
+}
