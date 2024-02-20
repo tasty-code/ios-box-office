1d86fd097c397627ce4100bfac9925bc0de4de2a
angryeon7 <nayeon7612@gmail.com>
2024-02-20 18:05:58 +0900

    feat: BoxOfficeService 정의

---
+++ BoxOffice/Network/Service/BoxOfficeService.swift
@@ -0,0 +1,47 @@
+//
+//  BoxOfficeService.swift
+//  BoxOffice
+//
+//  Created by nayeon  on 2/20/24.
+//
+
+import Foundation
+
+enum BoxOfficeService {
+    case requestDailyBoxOfficeInfo(userkey: String, date: String)
+}
+
+extension BoxOfficeService: TargetType {
+    
+    var method: HTTPMethod {
+        switch self {
+        case .requestDailyBoxOfficeInfo(_, _):
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .requestDailyBoxOfficeInfo(_, _):
+            return "/boxoffice/searchDailyBoxOfficeList.json"
+        }
+    }
+    
+    var parameters: RequestParameters {
+        switch self {
+        case .requestDailyBoxOfficeInfo(let userKey, let date):
+            let requestQuery: [String: Any] = [
+                "key": userKey,
+                "targetDt": date
+            ]
+            return .query(requestQuery)
+        }
+    }
+    
+    var header: HeaderType {
+        switch self {
+        case .requestDailyBoxOfficeInfo(_, _):
+            return .basic
+        }
+    }
+}
