ed677c8e02ae9a404343d26d5324c34b076cf9fc
angryeon7 <nayeon7612@gmail.com>
2024-02-20 18:13:43 +0900

    feat: BoxOfficeAPI 정의

---
+++ BoxOffice/Network/API/BoxOfficeAPI.swift
@@ -0,0 +1,40 @@
+//
+//  BoxOfficeAPI.swift
+//  BoxOffice
+//
+//  Created by nayeon  on 2/20/24.
+//
+
+import Foundation
+
+final class BoxOfficeAPI: BaseAPI {
+    
+    static let shared = BoxOfficeAPI(provider: NetworkProvider())
+    
+    private override init(provider: NetworkProvider) {
+        super.init(provider: provider)
+    }
+    
+    func requestDailyBoxOfficeAPI(userKey: String, date: String,
+                         completion: @escaping ((NetworkResult<Any>) -> Void)) {
+        guard let request = try? BoxOfficeService
+            .requestDailyBoxOfficeInfo(userkey: userKey, date: date)
+            .creatURLRequest()
+        else {
+            completion(.networkFail)
+            return
+        }
+        
+        provider.request(request) { result in
+            switch result {
+            case .success(let result):
+                let networkResult = self.judgeStatus(by: result.response.statusCode,
+                                                     result.data,
+                                                     BoxOfficeData.self)
+                completion(networkResult)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
