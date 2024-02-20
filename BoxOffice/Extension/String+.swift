f6079266c3a12ee9dc2a3deb1640fce9005ab7fb
eunjuchoi <eunjuzzoo@naver.com>
2024-02-20 17:44:51 +0900

    feat: 어제 날짜를 String 타입으로 변환해 가져오는 메서드 정의

---
+++ BoxOffice/Extension/String+.swift
@@ -0,0 +1,22 @@
+//
+//  String+.swift
+//  BoxOffice
+//
+//  Created by EUNJU on 2/20/24.
+//
+
+import Foundation
+
+extension String {
+    
+    /// 어제 날짜를 String 타입으로 변환해 가져오는 메서드
+    func convertYesterdayDateToString() -> String {
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyyMMdd"
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: date) ?? date
+        let result = format.string(from: yesterday)
+        
+        return result
+    }
+}
