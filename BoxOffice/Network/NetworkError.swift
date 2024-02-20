dd1851e57ae24fb187c856aebd7eb25b2c68e911
angryeon7 <nayeon7612@gmail.com>
2024-02-20 16:17:03 +0900

    feat: 네트워크에러 구현

---
+++ BoxOffice/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  BoxOffice
+//
+//  Created by nayeon  on 2/20/24.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case networkFailed
+}
