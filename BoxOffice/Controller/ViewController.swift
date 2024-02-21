101a12a3e8d1e368dcba56ad6b704fce3da2c1cd
Hoi.Kwon <teaho177@gmail.com>
2024-02-21 14:40:20 +0900

    refactor: loadJSONFromFile 호춯 확인

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -6,6 +6,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         Fetcher().fetchBoxOffice(targetDate: "20240219")
         Fetcher().fetchMovieInfo(movieCode: "20231415")
+        checkJSONLoader()
+    }
+    
+    func checkJSONLoader() {
+        do {
+            let movie: BoxOfficeResponse = try JSONLoader().loadJSONFromFile(from: .main, fileName: "box_office_sample")
+            dump(movie)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
