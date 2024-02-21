8199571df287086dcd797964582af995fc7c29cd
wlsdud-dev <hjymail1025@gmail.com>
2024-02-21 15:06:56 +0900

    feat: 일별 박스오피스 파싱 구현

--- BoxOffice/Network/NetworkManager.swift
+++ BoxOffice/Network/NetworkManager.swift
@@ -15,8 +15,33 @@ class NetworkManager {
     private init() {}
     
     // 일일 박스오피스 조회
-    func fetchDailyBoxOffice() {
+    func fetchDailyBoxOffice(for date: String, completion: @escaping (Result<[BoxOfficeMovieDTO], NetworkError>) -> Void) {
+        let urlString = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(apiKey)&targetDt=\(date)"
         
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
+        }
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let data = data,
+                  error == nil else {
+                completion(.failure(.requestFiled))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(BoxOfficeData.self, from: data)
+                let boxOfficeMovies = result.boxOfficeResult.dailyBoxOfficeList.map { boxOffice -> BoxOfficeMovieDTO in
+                    return BoxOfficeMovieDTO(rank: boxOffice.rank, movieName: boxOffice.movieName, openDate: boxOffice.openDate, audienceCount: boxOffice.audienceCount, movicode: boxOffice.movieCd)
+                }
+                completion(.success(boxOfficeMovies))
+            } catch {
+                completion(.failure(.requestFiled))
+            }
+        }
+        task.resume()
     }
     
     // 영화 상세 조회
