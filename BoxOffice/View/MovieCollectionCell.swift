b1c807b759ad01cc3ba8a2db74815a666888d14b
Matthew(매튜) <krsder@naver.com>
2024-02-21 15:25:39 +0900

    feat: CollectionCell 생성

--- BoxOffice/View/MovieCollectionCell.swift
+++ BoxOffice/View/MovieCollectionCell.swift
@@ -5,4 +5,15 @@
 //  Created by Matthew on 2/21/24.
 //
 
-import Foundation
+import UIKit
+
+class MovieCollectionCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
