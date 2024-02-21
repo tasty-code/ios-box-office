d48859ecae4a5088b9963220f79dd1925581d332
Matthew(매튜) <krsder@naver.com>
2024-02-21 15:25:15 +0900

    feat: ViewConctroller 설정

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -7,9 +7,33 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ViewController: UIViewController, DateFormattable {
 
+    let boxOfficeListView: UIView = BoxOfficeListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view = boxOfficeListView
+        configureNavigation()
+    }
+    
+    func configureNavigation() {
+        guard 
+            let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+        else {
+            return
+        }
+        title = makeDataFormatToString(date: yesterday, format: "yyyy-MM-dd")
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = MovieCollectionCell()
+        return cell
     }
 }
