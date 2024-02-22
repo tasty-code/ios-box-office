4ad2af17360481f0b82153383bac7134ca97367c
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-22 11:38:07 +0900

    feat: UINavigationController 생성

--- BoxOffice/App/SceneDelegate.swift
+++ BoxOffice/App/SceneDelegate.swift
@@ -19,8 +19,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return
         }
         window = UIWindow(windowScene: windowScene)
-        let navigationViewController = UINavigationController(rootViewController: ViewController())
-        
+        let navigationViewController = UINavigationController(rootViewController: ViewController(movieManager: movieManager))
         let mainViewController = navigationViewController
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
