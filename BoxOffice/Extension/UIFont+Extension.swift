b5fa28b1552d79f84a494631791b259390862a84
Changhyun Kang <changhyun.kyle@gmail.com>
2024-02-22 11:37:44 +0900

    feat: DynamicType + Fobnt.Weight 활용을 위한 UIFont Extension 구현

--- BoxOffice/Extension/UIFont+Extension.swift
+++ BoxOffice/Extension/UIFont+Extension.swift
@@ -5,4 +5,13 @@
 //  Created by 강창현 on 2/22/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let metrics = UIFontMetrics(forTextStyle: style)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        return metrics.scaledFont(for: font)
+    }
+}
