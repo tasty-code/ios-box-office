862050a695054eeab7f74c514cd56a87deced0c8
eensungkim <eensungkim@gmail.com>
2024-02-23 16:33:44 +0900

    refactor: alert 메서드가 Error 타입을 처리하도록 변경(NetworkError -> Error)

--- BoxOffice/Controller/AlertController.swift
+++ BoxOffice/Controller/AlertController.swift
@@ -9,8 +9,8 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    func alert(with error: NetworkError) {
-        let message = error.errorDescription
+    func alert(with error: Error) {
+        let message = error.localizedDescription
         let alertViewController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default)
         alertViewController.addAction(action)
