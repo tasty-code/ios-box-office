e69d728717cace11ba61c891a91b3dde1f0011b7
LeeSe0ngYe0n <goblin__@naver.com>
2024-02-23 16:02:24 +0900

    feat: invalidURL 케이스 추가

--- BoxOffice/Network/Error/NetworkError.swift
+++ BoxOffice/Network/Error/NetworkError.swift
@@ -9,11 +9,14 @@ import Foundation
 
 enum NetworkError: LocalizedError {
     case invalidStatusCode(Int)
+    case invalidURL
     
     var errorDescription: String {
         switch self {
         case .invalidStatusCode(let statusCode):
             "일시적인 \(statusCode) 에러로 데이터를 불러오지 못했습니다."
+        case .invalidURL:
+            "입력한 URL이 잘못되었습니다."
         }
     }
 }
