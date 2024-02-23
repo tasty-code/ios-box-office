e7e4580f6c02f1024584ab3651be503b35014652
eensungkim <eensungkim@gmail.com>
2024-02-23 16:34:17 +0900

    refactor: localizedDescription 으로 변수명 변경

--- BoxOffice/Network/Error/NetworkError.swift
+++ BoxOffice/Network/Error/NetworkError.swift
@@ -10,13 +10,16 @@ import Foundation
 enum NetworkError: LocalizedError {
     case invalidStatusCode(Int)
     case invalidURL
+    case invalidURLResponse
     
-    var errorDescription: String {
+    var localizedDescription: String {
         switch self {
         case .invalidStatusCode(let statusCode):
             "일시적인 \(statusCode) 에러로 데이터를 불러오지 못했습니다."
         case .invalidURL:
             "입력한 URL이 잘못되었습니다."
+        case .invalidURLResponse:
+            "전달받은 URLResponse 가 적합한 URLResponse 가 아닙니다."
         }
     }
 }
