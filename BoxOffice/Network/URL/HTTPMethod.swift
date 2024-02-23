f2af3d4abc0a8591a94782b8f99b3fa6c98e5ac0
eensungkim <eensungkim@gmail.com>
2024-02-23 17:28:47 +0900

    feat: HTTPMethod 열거형 추가

---
+++ BoxOffice/Network/URL/HTTPMethod.swift
@@ -0,0 +1,20 @@
+//
+//  HTTPMethod.swift
+//  BoxOffice
+//
+//  Created by Kim EenSung on 2/23/24.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case head = "HEAD"
+    case post = "POST"
+    case put = "PUT"
+    case DELETE = "DELETE"
+    case connect = "CONNECT"
+    case options = "OPTIONS"
+    case trace = "TRACE"
+    case patch = "PATCH"
+}
