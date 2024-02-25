9c757fe4f506372fcb2d3d2c6821ea6244dad621
angryeon7 <nayeon7612@gmail.com>
2024-02-26 01:31:17 +0900

    feat: BoxOfficeViewController UI 구현

--- BoxOffice/Presentation/BoxOfficeViewController.swift
+++ BoxOffice/Presentation/BoxOfficeViewController.swift
@@ -8,4 +8,41 @@
 import UIKit
 
 class BoxOfficeViewController: UIViewController {
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+        setupConstraint()
+        setupNavigationBar()
+    }
+}
+
+extension BoxOfficeViewController {
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+    }
+
+    func setupConstraint() {
+        let safaArea = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: safaArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safaArea.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: safaArea.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safaArea.bottomAnchor)
+        ])
+    }
+
+    func setupNavigationBar() {
+
+    }
 }
