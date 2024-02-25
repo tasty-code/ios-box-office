1088f5bfebbb6b5a1dd78f58375bb59b90d1ed58
angryeon7 <nayeon7612@gmail.com>
2024-02-26 01:45:52 +0900

    feat: BoxOfficeCollectionViewCell UI 구현

---
+++ BoxOffice/Presentation/BoxOfficeCollectionViewCell.swift
@@ -0,0 +1,122 @@
+//
+//  BoxOfficeCollectionViewCell.swift
+//  BoxOffice
+//
+//  Created by nayeon  on 2/26/24.
+//
+
+import UIKit
+
+class BoxOfficeCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "BoxOfficeCollectionViewCell"
+
+    private let rankLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        return label
+    }()
+    
+    private let detailLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let rankChangeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var rankStactkView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [rankLabel, rankChangeLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = 3
+        return stackView
+    }()
+    
+    private lazy var titleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, detailLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private let accessoryButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+}
+
+extension BoxOfficeCollectionViewCell {
+    func setupUI() {
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.borderWidth = 0.2
+    }
+    
+    func configureUI() {
+        contentView.addSubview(titleStackView)
+        contentView.addSubview(rankStactkView)
+        contentView.addSubview(accessoryButton)
+    }
+
+    func setupConstraint() {
+        setupRankStackVeiwConstraint()
+        setupMovieInfomationStackViewConstraint()
+        setupAccessoryButtonViewConstraint()
+    }
+
+    func setupRankStackVeiwConstraint() {
+        NSLayoutConstraint.activate([
+            rankStactkView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            rankStactkView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            rankStactkView.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+
+    func setupMovieInfomationStackViewConstraint() {
+        NSLayoutConstraint.activate([
+            titleStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            titleStackView.leadingAnchor.constraint(equalTo: rankStactkView.trailingAnchor, constant: 30),
+            titleStackView.trailingAnchor.constraint(equalTo: accessoryButton.leadingAnchor, constant: -20)
+        ])
+    }
+
+    func setupAccessoryButtonViewConstraint() {
+        NSLayoutConstraint.activate([
+            accessoryButton.widthAnchor.constraint(equalToConstant: 20),
+            accessoryButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            accessoryButton.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
