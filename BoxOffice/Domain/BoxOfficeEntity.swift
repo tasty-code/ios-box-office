3ec13f905352bc89a0ad741c1d92bd9df0788e18
angryeon7 <nayeon7612@gmail.com>
2024-02-26 02:04:18 +0900

    feat: BoxOfficeEntity 생성

---
+++ BoxOffice/Domain/BoxOfficeEntity.swift
@@ -0,0 +1,15 @@
+//
+//  BoxOfficeEntity.swift
+//  BoxOffice
+//
+//  Created by nayeon  on 2/26/24.
+//
+
+struct BoxOfficeEntity {
+    let rank: String
+    let movieName: String
+    let salesAmount: String
+    let audienceCount: String
+    let rankChangeValue: String
+    let isNewMovie: Bool
+}
