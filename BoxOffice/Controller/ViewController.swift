98919067ad2b5b4dcddf069684e4fd1e7bcd6231
Hoi.Kwon <teaho177@gmail.com>
2024-02-27 13:00:47 +0900

    refactor: Fetch객체 및 메서드 네이밍 수정, 생성자 구현

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -4,8 +4,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Fetcher().fetchBoxOffice(targetDate: "20240219")
-        Fetcher().fetchMovieInfo(movieCode: "20231415")
+        MovieAPIFetcher().fetchBoxOffice(targetDate: "20240219")
+        MovieAPIFetcher().fetchdetailMovieInfo(movieCode: "20231415")
         checkJSONLoader()
     }
     
