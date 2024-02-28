4d45369089e8cf41f7e8838c10cbe82d3089a004
yoruck2 <yoruck2@gmail.com>
2024-02-28 22:58:35 +0900

    feat: NetworkAPI 생성

---
+++ BoxOffice/Network/NetworkAPI.swift
@@ -0,0 +1,65 @@
+//
+//  NetworkAPI.swift
+//  BoxOffice
+//
+//  Created by dopamint on 2/17/24.
+//
+
+import Foundation
+
+struct NetworkAPI {
+    
+    func buildDailyBoxOfficeAPI(targetDate: APIURLCompnents.QueryValues) -> URL? {
+        return buildUrl(apiType: .dailyBoxOffice, query: targetDate).url
+    }
+    
+    func buildMovieDetailAPI(movieCode: APIURLCompnents.QueryValues) -> URL? {
+        return buildUrl(apiType: .movieDetail, query: movieCode).url
+    }
+    
+    private func buildUrl(apiType: APIType, query: APIURLCompnents.QueryValues) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = APIURLCompnents.schema
+        components.host = APIURLCompnents.host
+        components.path = apiType.makePath()
+        
+        components.queryItems = [
+            URLQueryItem(name: "key", value: APIURLCompnents.QueryValues.key.rawValue),
+            URLQueryItem(name: apiType.rawValue , value: query.rawValue)
+        ]
+        return components
+    }
+}
+
+extension NetworkAPI {
+    func buildDailyBoxOfficeAPI(targetDate: APIURLCompnents.QueryValues, keys: APIURLCompnents.QueryKeys..., values: Any...) -> URL? {
+        let dict: [APIURLCompnents.QueryKeys: Any] = zip(keys, values).reduce(into: [:]) { partialResult, now in
+            partialResult[now.0] = now.1
+        }
+        return buildUrl(apiType: .dailyBoxOffice, requiredQuery: targetDate, queries: dict).url
+    }
+    
+    func buildMovieDetailAPI(movieCode: APIURLCompnents.QueryValues, keys: APIURLCompnents.QueryKeys..., values: Any...) -> URL? {
+        let dict: [APIURLCompnents.QueryKeys: Any] = zip(keys, values).reduce(into: [:]) { partialResult, now in
+            partialResult[now.0] = now.1 as! Dictionary<APIURLCompnents.QueryKeys, Any>.SubSequence
+        }
+        return buildUrl(apiType: .movieDetail,requiredQuery: movieCode, queries: dict).url
+    }
+    
+    private func buildUrl(apiType: APIType, requiredQuery: APIURLCompnents.QueryValues, queries: [APIURLCompnents.QueryKeys: Any]) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = APIURLCompnents.schema
+        components.host = APIURLCompnents.host
+        components.path = apiType.makePath()
+
+        components.queryItems = [
+            URLQueryItem(name: "key", value: APIURLCompnents.QueryValues.key.rawValue),
+            URLQueryItem(name: apiType.rawValue , value: requiredQuery.rawValue)
+        ]
+        queries.forEach { (key: APIURLCompnents.QueryKeys, value: Any) in
+            components.queryItems?.append(URLQueryItem(name: key.rawValue,
+                                                       value: value as? String))
+        }
+        return components
+    }
+}
