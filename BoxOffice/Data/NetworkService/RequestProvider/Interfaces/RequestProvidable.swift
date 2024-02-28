90f491c49e268f2ec9357bb475b4506370af5635
hemil0102 <hemil0102@gmail.com>
2024-02-28 14:43:14 +0900

    refactor: RequestProvidable extension 불필요한 코드 삭제

--- BoxOffice/Data/NetworkService/RequestProvider/Interfaces/RequestProvidable.swift
+++ BoxOffice/Data/NetworkService/RequestProvider/Interfaces/RequestProvidable.swift
@@ -2,10 +2,5 @@
 import Foundation
 
 protocol RequestProvidable {
-    var baseURL: BaseURLType { get }
     func makeURLRequest(for boxOfficeType: URLProvider) -> URLRequest?
-}
-
-extension RequestProvidable {
-    var baseURL: BaseURLType { .kobis }
 }
