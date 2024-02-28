cb0305107dae589c2b4eb506cc67de7e6ba5dfb4
yoruck2 <yoruck2@gmail.com>
2024-02-28 22:58:19 +0900

    feat: JSONParser 생성

---
+++ BoxOffice/Network/JSONParser.swift
@@ -0,0 +1,22 @@
+//
+//  JSONParser.swift
+//  BoxOffice
+//
+//  Created by dopamint on 2/20/24.
+//
+
+import Foundation
+
+struct JSONParser{
+
+    func parseJSON<T: Decodable>(_ movieData: Data, DTO: T.Type) -> T? {
+        do {
+            let decodedData = try JSONDecoder().decode(T.self, from: movieData)
+            return decodedData
+        } catch {
+            print("파싱 에러")
+            return nil
+        }
+    }
+}
+
