e5f7f948c71c69419fe2160080f7cdc3bf71b5e5
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-02-28 09:27:12 +0900

    refactor: 상태 코드 네이밍 수정 및 NetworkManager 수정

--- BoxOffice/Sources/Model/NetworkManager.swift
+++ BoxOffice/Sources/Model/NetworkManager.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct NetworkManager<T: Decodable> {
+struct NetworkManager<T: Decodable>: StatusCodeProtocol {
+    
     private let networkSession: NetworkSessionProtocol
     private let networkRequestBuilder: NetworkRequestBuilderProtocol
     
@@ -19,26 +20,29 @@ struct NetworkManager<T: Decodable> {
         self.networkRequestBuilder = networkRequestBuilder
     }
     
-    private func requestBuilder() {
-        
-    }
-    
-    mutating func request(complection: @escaping (Data) -> Void) {
+    mutating func request(complection: @escaping (T) -> Void) {
         let builder = networkRequestBuilder
         
         guard let request = builder.setURLRequest() else {
             return
         }
-//        let kobisSession = networkSession as! NetworkSession
-        networkSession.dataTask(with: request, complection: { result in
+        
+        networkSession.dataTask(with: request, complection: { [self] result in
             switch result {
             case .success(let networkResponse):
+                let decoder = JsonDecoder.shared
                 guard let networkResponse = networkResponse as? NetworkResponse,
-                      let data = networkResponse.data
+                      let data = networkResponse.data,
+                      let decodeData: T = try? decoder.decode(T.self, data)
                 else {
                     return
                 }
-                complection(data)
+                
+                if !success.contains(networkResponse.response.statusCode) {
+                    return
+                }
+                
+                complection(decodeData)
             case .failure(let error):
                 print(error.localizedDescription)
             }
--- BoxOffice/Sources/Network/Response/Interface/StatusCodeProtocol.swift
+++ BoxOffice/Sources/Network/Response/Interface/StatusCodeProtocol.swift
@@ -10,16 +10,16 @@ import Foundation
 protocol StatusCodeProtocol {
     var information: Range<Int> { get }
     var success: Range<Int> { get }
-    var redirectRange: Range<Int> { get }
-    var clientErrorRange: Range<Int> { get }
-    var serverErrorRange: Range<Int> { get }
+    var redirect: Range<Int> { get }
+    var clientError: Range<Int> { get }
+    var serverError: Range<Int> { get }
 }
 
 extension StatusCodeProtocol {
     var information: Range<Int> { (100..<200) }
     var success: Range<Int> { 200..<300 }
-    var redirectRange: Range<Int> { 300..<400 }
-    var clientErrorRange: Range<Int> { 400..<500 }
-    var serverErrorRange: Range<Int> { 500..<600 }
+    var redirect: Range<Int> { 300..<400 }
+    var clientError: Range<Int> { 400..<500 }
+    var serverError: Range<Int> { 500..<600 }
 }
 
