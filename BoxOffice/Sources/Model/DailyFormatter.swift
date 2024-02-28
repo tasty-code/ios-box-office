a73b8f9a2ac61c9971358f7886146520d40460eb
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-02-28 09:27:54 +0900

    feat: DailyFormatter 프로토콜 extension 추가

--- BoxOffice/Sources/Model/DailyFormatter.swift
+++ BoxOffice/Sources/Model/DailyFormatter.swift
@@ -10,3 +10,13 @@ import Foundation
 protocol DailyFormatter {
     func dailyFormatter(format: String) -> String
 }
+
+extension DailyFormatter {
+    func dailyFormatter(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+
+        let current_date_string = formatter.string(from: Date() - 86400)
+        return current_date_string
+    }
+}
