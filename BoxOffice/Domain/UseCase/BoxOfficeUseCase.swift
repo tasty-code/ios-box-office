7ff1c11bef149b3dddaabbe116c5a3f560e47575
jus1234 <osjamg1996@gmail.com>
2024-02-28 15:31:49 +0900

    refactor: Domain 리팩토링

---
+++ BoxOffice/Domain/UseCase/BoxOfficeUseCase.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+protocol BoxOfficeUseCase {
+    func fetch(completion: @escaping (Result<BoxOffice, Error>) -> Void) -> URLSessionTask?
+}
+
+final class DefaulBoxOfficeUseCase {
+    private let boxOfficeRepository: BoxOfficeRepository
+    
+    init(boxOfficeRepository: BoxOfficeRepository) {
+        self.boxOfficeRepository = boxOfficeRepository
+    }
+}
+
+extension DefaulBoxOfficeUseCase: BoxOfficeUseCase {
+    func fetch(completion: @escaping (Result<BoxOffice, Error>) -> Void) -> URLSessionTask? {
+        return boxOfficeRepository.fetchBoxOfficeList { result in
+            completion(result)
+        }
+    }
+}
