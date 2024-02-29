c86bc6a4610b342acf88fc4751d69049fa6d0f94
jus1234 <osjamg1996@gmail.com>
2024-02-29 14:52:14 +0900

    feat: MovieListCollectionViewController, MovieListViewModel 바인딩

--- BoxOffice/Presentation/MoviesList/View/MoviesListViewController.swift
+++ BoxOffice/Presentation/MoviesList/View/MoviesListViewController.swift
@@ -1,27 +1,26 @@
 import UIKit
 
-class MoviesCollectionView: UIViewController {
+final class MoviesListViewController: UIViewController {
     
-    let viewModel: MoviesListViewModel
+    private let viewModel: MoviesListViewModel
     
-    lazy var collectionView: UICollectionView = {
+    private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(MoviesListCell.self, forCellWithReuseIdentifier: "MoviesListCell")
+        collectionView.register(MoviesListCell.self, forCellWithReuseIdentifier: String(describing: MoviesListCell.self))
         return collectionView
     }()
     
     let refreshControl = UIRefreshControl()
-    var isRefreshing = false
+    private var isRefreshing = false
     
     init(viewModel: MoviesListViewModel, isRefreshing: Bool = false) {
         self.viewModel = viewModel
         self.isRefreshing = isRefreshing
-        print("생성자")
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -31,67 +30,54 @@ class MoviesCollectionView: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
         setupCollectionView()
         setupRefreshControl()
         setupNavigationBar()
+        bind()
         fetchData()
     }
     
-    func setupNavigationBar() {
+}
+
+extension MoviesListViewController {
+    
+    private func bind() {
+        viewModel.movies.bind { _ in
+            self.reload()
+        }
+        viewModel.errorMessage.bind { errorMessage in
+            self.makeAlert(message: errorMessage, confirmAction: nil)
+        }
+    }
+    
+    private func setupNavigationBar() {
         navigationItem.title = "\(DateFormatter.titleDateFormatter.string(from: Date().yesterday))"
     }
     
-    func setupCollectionView() {
-        view.addSubview(collectionView)
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-    }
-    
-    func setupRefreshControl() {
+    private func setupRefreshControl() {
         collectionView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
     }
     
-    @objc func refreshData() {
+    @objc private func refreshData() {
         if !isRefreshing {
             isRefreshing = true
             fetchData()
         }
     }
     
-    func fetchData() {
-        viewModel.fetchData { [weak self] result in
-            switch result {
-            case .success(let movies):
-                print("fetchData: \(movies)")
-                DispatchQueue.main.async {
-                    self?.collectionView.reloadData()
-                    self?.refreshControl.endRefreshing()
-                    self?.isRefreshing = false
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-                DispatchQueue.main.async {
-                    self?.refreshControl.endRefreshing()
-                    self?.isRefreshing = false
-                }
-            }
-        }
+    private func fetchData() {
+        viewModel.fetchData()
     }
 }
 
-extension MoviesCollectionView: UICollectionViewDataSource {
+extension MoviesListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.movies.value.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MoviesListCell", for: indexPath) as? MoviesListCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: MoviesListCell.self), for: indexPath) as? MoviesListCell else {
             fatalError("MoviesListCell 에러")
         }
         let movie = viewModel.movies.value[indexPath.item]
@@ -99,19 +85,35 @@ extension MoviesCollectionView: UICollectionViewDataSource {
         return cell
     }
     
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let selectedMovie = viewModel.movies.value[indexPath.item]
-//        showMovieDetailScreen(for: selectedMovie)
-//    }
     func showMovieDetailScreen(for movie: MovieBoxOffice) {
         let movieDetailViewController = MovieDetailView(movie: movie)
         navigationController?.pushViewController(movieDetailViewController, animated: true)
     }
+    
+    func reload() {
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+            self.isRefreshing = false
+            self.refreshControl.endRefreshing()
+        }
+    }
 }
 
-extension MoviesCollectionView: UICollectionViewDelegateFlowLayout {
+extension MoviesListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.bounds.width
         return CGSize(width: width, height: 100)
+    }
+}
+
+extension MoviesListViewController {
+    private func setupCollectionView() {
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
