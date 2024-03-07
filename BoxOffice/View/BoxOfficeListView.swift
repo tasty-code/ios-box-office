e3ecce002883f9c98f1d8e136fc1ff282b2201d6
Hoi.Kwon <teaho177@gmail.com>
2024-03-07 17:26:50 +0900

    feat: CollectionView 구현

---
+++ BoxOffice/View/BoxOfficeListView.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+protocol BoxOfficeListDelegate: AnyObject {
+    func refreshBoxOfficeData()
+}
+
+class BoxOfficeListView: UICollectionView {
+    weak var movieListDelegate: BoxOfficeListDelegate?
+    let loadingIndicator = UIActivityIndicatorView(style: .medium)
+    
+    private lazy var pullToRefreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addAction(UIAction { [weak self] _ in self?.refreshBoxOfficeCollectionView() }, for: .valueChanged)
+        return refreshControl
+    }()
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        setupCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCollectionView() {
+        registerCells()
+        configureLoadingIndicator()
+        self.refreshControl = pullToRefreshControl
+    }
+    
+    private func registerCells() {
+        self.register(MovieListCollectionViewCell.self, forCellWithReuseIdentifier: MovieListCollectionViewCell.reuseIdentifier)
+    }
+    
+    private func configureLoadingIndicator() {
+        loadingIndicator.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(loadingIndicator)
+        NSLayoutConstraint.activate([
+            loadingIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            loadingIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        self.isScrollEnabled = true
+    }
+    
+    private func refreshBoxOfficeCollectionView() {
+            self.movieListDelegate?.refreshBoxOfficeData()
+    }
+}
