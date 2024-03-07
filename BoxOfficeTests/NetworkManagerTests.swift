766146ec8813ff09f88b259ff38de1677456adc6
LeeSe0ngYe0n <goblin__@naver.com>
2024-03-07 20:10:49 +0900

    refactor: 리뷰변경 사항에 따라 유닛테스트 코드 수정

--- BoxOfficeTests/NetworkManagerTests.swift
+++ BoxOfficeTests/NetworkManagerTests.swift
@@ -17,13 +17,13 @@ final class NetworkManagerTests: XCTestCase {
         sut = NetworkManager(urlSession: mockSession)
     }
     
-    func test_makeRequest메서드는_기본적으로GET메서드를_반환한다() throws {
+    func test_makeRequest메서드는_http메서드를전달하지않으면_get메서드를반환한다() throws {
         // given
         let input: KoreanFilmCouncilURL = .dailyBoxOffice(targetDate: "20120419")
         let expectation = "GET"
         
         // when
-        let request = try sut?.makeRequest(input.url)
+        let request = sut?.makeRequest(input.url)
         
         // then
         XCTAssertEqual(request?.httpMethod, expectation)
@@ -31,28 +31,43 @@ final class NetworkManagerTests: XCTestCase {
     
     func test_makeRequest메서드는_알맞은URL을_반환한다() throws {
         // given
-        let input: KoreanFilmCouncilURL = .dailyBoxOffice(targetDate: "20120419")
-        let expectation = "searchDailyBoxOfficeList.json"
+        let input: String = KoreanFilmCouncilURL.dailyBoxOffice(targetDate: "20120419").url
+        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else {
+            return XCTFail()
+        }
+        let expectation = URL(string: KoreanFilmCouncilURL.dailyBoxOffice(targetDate: "20120419").url)
         
         // when
-        let request = try sut?.makeRequest(input.url)
+        let request = sut?.makeRequest(input)
         
         // then
-        XCTAssertEqual(request?.url?.lastPathComponent, expectation)
+        XCTAssertEqual(request?.url, expectation)
     }
     
     func test_request메서드는_parsing된데이터를_반환한다() async throws {
         // given
-        let input: KoreanFilmCouncilURL = .dailyBoxOffice(targetDate: "20220105")
-        guard let request = try sut?.makeRequest(input.url) else {
-            return XCTFail()
-        }
+        let input: String = KoreanFilmCouncilURL.dailyBoxOffice(targetDate: "20220105").url
         
         // when
-        let result: BoxOfficeResult? = try await sut?.request(request)
+        let result: BoxOfficeResult? = try await sut?.request(input)
         
         // then
         XCTAssertNotNil(result)
+    }
+    
+    func test_request메서드는_잘못된URL이입력될경우_에러를반환한다() async {
+        // given
+        let input: String = "잘못된 URL"
+        let expectation = XCTestExpectation(description: "잘못된 URL은 에러를 반환한다.")
         
+        // when
+        do {
+            let _: BoxOfficeResult? = try await sut?.request(input)
+        } catch {
+            
+            // then
+            XCTAssertEqual(error as? NetworkError, NetworkError.invalidURL)
+            expectation.fulfill()
+        }
     }
 }
