c87cdeb5966d71e71e75139c70337347be7b9356
yoruck2 <yoruck2@gmail.com>
2024-03-08 19:22:25 +0900

    feat: HTTPMethod enum추가

--- BoxOffice/Network/HTTPMethod.swift
+++ BoxOffice/Network/HTTPMethod.swift
@@ -5,4 +5,10 @@
 //  Created by dopamint on 3/8/24.
 //
 
-import Foundation
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
