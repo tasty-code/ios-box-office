630ea9d8e9079f0821faca85c39bfd9a9c413be4
suojae3 <suojae3@gmail.com>
2024-03-08 11:08:31 +0900

    feat: numberFormatter 기능 추가

--- BoxOffice/Presentation/Common/ViewControllerExtension.swift
+++ BoxOffice/Presentation/Common/ViewControllerExtension.swift
@@ -2,6 +2,15 @@
 import UIKit
 
 extension UIViewController {
+        
+    // MARK: - Handle Number
+    func numberFormatter(for data: String) -> String {
+        let numberFormatter: NumberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        guard let result = numberFormatter.string(from: NSNumber(value: Double(data) ?? 0)) else { return "error" }
+        return result
+    }
+
     
     // MARK: - Alert Controller
     func presentAlert(title: String,
