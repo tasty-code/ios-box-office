ca65fec0aa915ac92c19c74af0bc4ebfb3afd561
hemil0102 <hemil0102@gmail.com>
2024-03-08 11:00:58 +0900

    feat: BoxOfficeCollectionView 생성

---
+++ BoxOffice/Presentation/DisplayModel/BoxOfficeCollectionView.swift
@@ -0,0 +1,35 @@
+
+import UIKit
+
+class BoxOfficeCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: BoxOfficeCollectionView.createLayout())
+        configureCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureCollectionView() {
+        self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.backgroundColor = .systemBackground
+    }
+
+    private static func createLayout() -> UICollectionViewLayout {
+        let estimatedHeight = CGFloat(78)
+        let layoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(estimatedHeight))
+        let item = NSCollectionLayoutItem(layoutSize: layoutSize)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: layoutSize,
+                                                       subitem: item,
+                                                       count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        section.interGroupSpacing = 0
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
--- BoxOffice/Presentation/View/BoxOfficeCell.swift
+++ BoxOffice/Presentation/View/BoxOfficeCell.swift
@@ -63,8 +63,6 @@ final class BoxOfficeCell: UICollectionViewListCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
 }
 
 extension BoxOfficeCell {
@@ -76,7 +74,6 @@ extension BoxOfficeCell {
         configureSeparatorView()
     }
     
-
     private func configureLabels() {
         [rankLabel, rankIntensityLabel, movieNameLabel, audienceAccountLabel].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
