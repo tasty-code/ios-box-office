75de16ca21bb15e2ec0ce705b4e0b578b5e30dd8
Changhyun Kang <changhyun.kyle@gmail.com>
2024-03-08 11:53:39 +0900

    chore: numberFormatter -> 연산프로퍼티로 변경

--- BoxOffice/Extension/Int+Extension.swift
+++ BoxOffice/Extension/Int+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Int {
-    func numberFormatter() -> String {
+    var numberFormatter: String {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
         guard
