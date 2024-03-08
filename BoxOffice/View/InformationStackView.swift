98ef6cf1442c11bcdbcc1219b04618f52e03fbfa
eensungkim <eensungkim@gmail.com>
2024-03-08 21:45:56 +0900

    feat: 중복 관리를 위한 InformationStackView 클래스 생성

---
+++ BoxOffice/View/InformationStackView.swift
@@ -0,0 +1,40 @@
+//
+//  InformationStackView.swift
+//  BoxOffice
+//
+//  Created by Kim EenSung on 3/8/24.
+//
+
+import UIKit
+
+class InformationStackView: UIStackView {
+    let titleLabel: UILabel = UILabel(font: .preferredFont(forTextStyle: .headline), textAlignment: .center)
+    let contentLabel: UILabel = UILabel(font: .preferredFont(forTextStyle: .body), textAlignment: .left)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTitleText(_ text: String) -> Self {
+        titleLabel.text = text
+        return self
+    }
+    
+    private func setConstraints() {
+        self.addArrangedSubview(titleLabel)
+        self.addArrangedSubview(contentLabel)
+        
+        contentLabel.numberOfLines = 0
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.2).isActive = true
+        contentLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8).isActive = true
+    }
+}
