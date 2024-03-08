4150155cb92a8826019ade69dee73b69c1f4a6d2
suojae3 <suojae3@gmail.com>
2024-03-08 12:00:36 +0900

    refactor: BoxOfficeViewController 리팩토링

--- BoxOffice/Presentation/Controller/BoxOfficeViewController.swift
+++ BoxOffice/Presentation/Controller/BoxOfficeViewController.swift
@@ -5,7 +5,7 @@ final class BoxOfficeViewController: UIViewController {
     
     private let boxOfficeUseCase: BoxOfficeUseCaseProtocol
     
-    @SynchronizedLock private var movies: [BoxOfficeDisplayModel] = []
+    @SynchronizedLock private var movies = [BoxOfficeDisplayModel]()
     private var fetchTask: Task<Void, Never>?
     
     private var boxOfficeCollectionView: BoxOfficeCollectionView!
@@ -16,7 +16,7 @@ final class BoxOfficeViewController: UIViewController {
         self.boxOfficeUseCase = boxOfficeUseCase
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) { fatalError() }
     
     deinit {
@@ -31,18 +31,57 @@ extension BoxOfficeViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
         configureDataSource()
         fetchBoxOfficeData()
-        setupRefreshControl()
     }
 }
 
 
-// MARK: - Refresh
+// MARK: - Setup UI
 private extension BoxOfficeViewController {
     
+    func setupUI() {
+        setupBoxOfficeView()
+        configureCellRegistration()
+        configureNavigationBar()
+        setupRefreshControl()
+    }
+    
+    func configureNavigationBar() {
+        navigationItem.title = Date().formattedDate(withFormat: "YYYY-MM-dd")
+    }
+    
+    // 커스텀 뷰 설정
+    func setupBoxOfficeView() {
+        boxOfficeCollectionView = BoxOfficeCollectionView(frame: .zero)
+        view.backgroundColor = boxOfficeCollectionView.backgroundColor
+        view.addSubview(boxOfficeCollectionView)
+        boxOfficeCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            boxOfficeCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            boxOfficeCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            boxOfficeCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            boxOfficeCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+        
+        configureCellRegistration()
+    }
+    
+    // 셀 등록 설정 메서드
+    func configureCellRegistration() {
+        cellRegistration = UICollectionView.CellRegistration<BoxOfficeCell, BoxOfficeDisplayModel> { (cell, indexPath, movie) in
+            cell.accessories = [.disclosureIndicator()]
+            cell.rankLabel.text = movie.rank
+            cell.movieNameLabel.text = movie.movieName
+            guard let rankIntensity = Int(movie.rankIntensity) else { return }
+            cell.matchRankIntensity(of: movie.isNew, with: rankIntensity)
+            cell.matchAudienceAccount(of: movie)
+        }
+    }
+    
+    // Refresh
     func setupRefreshControl() {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshBoxOfficeData), for: .valueChanged)
@@ -55,53 +94,9 @@ private extension BoxOfficeViewController {
 }
 
 
-// MARK: - Setup UI
-private extension BoxOfficeViewController {
-    
-    private func setupUI() {
-        setupBoxOfficeView()
-        configureCellRegistration()
-        configureNavigationBar()
-    }
-    
-    private func configureNavigationBar() {
-        navigationItem.title = Date().formattedDate(withFormat: "YYYY-MM-dd")
-    }
-    
-    // 커스텀 뷰 설정
-    private func setupBoxOfficeView() {
-        boxOfficeCollectionView = BoxOfficeCollectionView(frame: .zero)
-        view.backgroundColor = boxOfficeCollectionView.backgroundColor
-        boxOfficeCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(boxOfficeCollectionView)
-        
-        NSLayoutConstraint.activate([
-            boxOfficeCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            boxOfficeCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            boxOfficeCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            boxOfficeCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-        ])
-        
-        configureCellRegistration()
-    }
-
-    // 셀 등록 설정 메서드
-    private func configureCellRegistration() {
-        cellRegistration = UICollectionView.CellRegistration<BoxOfficeCell, BoxOfficeDisplayModel> { (cell, indexPath, movie) in
-            cell.accessories = [.disclosureIndicator()]
-            cell.rankLabel.text = movie.rank
-            cell.movieNameLabel.text = movie.movieName
-            guard let rankIntensity = Int(movie.rankIntensity) else { return }
-            cell.matchRankIntensity(of: movie.isNew, with: rankIntensity)
-            cell.matchAudienceAccount(of: movie)
-        }
-    }
-}
-
-
 // MARK: - Fetch Data
 private extension BoxOfficeViewController {
-
+    
     func fetchBoxOfficeData() {
         fetchTask = Task {
             let result = await boxOfficeUseCase.fetchBoxOfficeData()
@@ -109,6 +104,7 @@ private extension BoxOfficeViewController {
         }
     }
     
+    @MainActor
     func handleFetchResult(_ result: Result<[BoxOfficeMovie], DomainError>) {
         boxOfficeCollectionView.refreshControl?.endRefreshing()
         switch result {
@@ -117,10 +113,10 @@ private extension BoxOfficeViewController {
             self.movies = displayMovies
             applySnapshot(movies: displayMovies, animatingDifferences: true)
         case .failure(let error):
-            print("Failed to load data: \(error)")
+            presentAlert(title: "네트워크 오류", message: "네트워크에 문제가 있습니다", confirmTitle: "확인")
         }
     }
-
+    
     func mapEntityToDisplayModel(_ boxOfficeMovies: [BoxOfficeMovie]) -> [BoxOfficeDisplayModel] {
         return boxOfficeMovies.map {
             BoxOfficeDisplayModel(
@@ -133,22 +129,6 @@ private extension BoxOfficeViewController {
     }
 }
 
-
-// MARK: - Update UI
-private extension BoxOfficeViewController {
-    // 결과에 따라 UI 업데이트
-    private func updateUI(with result: Result<[BoxOfficeDisplayModel], DomainError>, isLoading: Bool = false) {
-        switch result {
-        case .success(let movies):
-            self.movies = movies
-            applySnapshot(movies: movies, animatingDifferences: true)
-        case .failure(let error):
-            print("Failed to load data: \(error)")
-        }
-    }
-}
-
-
 // MARK: - Apply Diffable DataSource
 private extension BoxOfficeViewController {
     
@@ -157,14 +137,14 @@ private extension BoxOfficeViewController {
             (collectionView, indexPath, movie) -> UICollectionViewCell? in
             return collectionView.dequeueConfiguredReusableCell(using: self.cellRegistration, for: indexPath, item: movie)
         }
-        let loadingPlaceholder = (1...10).map { _ in BoxOfficeDisplayModel.placeholder }
+        let loadingPlaceholder = [BoxOfficeDisplayModel.placeholder]
         var initialSnapshot = NSDiffableDataSourceSnapshot<Section, BoxOfficeDisplayModel>()
         initialSnapshot.appendSections([.main])
         initialSnapshot.appendItems(loadingPlaceholder, toSection: .main)
         dataSource.apply(initialSnapshot, animatingDifferences: false) // 초기 스냅샷 적용
     }
     
-    private func applySnapshot(movies: [BoxOfficeDisplayModel], animatingDifferences: Bool) {
+    func applySnapshot(movies: [BoxOfficeDisplayModel], animatingDifferences: Bool) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, BoxOfficeDisplayModel>()
         snapshot.appendSections([.main])
         snapshot.appendItems(movies, toSection: .main)
