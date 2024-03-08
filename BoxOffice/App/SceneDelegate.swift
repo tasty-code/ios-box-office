e63b43789b1c080bb81cc6788dd788fd426d9314
suojae3 <suojae3@gmail.com>
2024-03-08 15:40:36 +0900

    .

--- BoxOffice/App/SceneDelegate.swift
+++ BoxOffice/App/SceneDelegate.swift
@@ -25,3 +25,54 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         BaseURLManager.shared.configure(url, for: .kobis)
     }
 }
+
+protocol EndPointMakable {
+    var url: URL? { get }
+}
+
+struct EndPoint: EndPointMakable {
+    init(urlInformation: URLInformation) {
+        self.urlInformation = urlInformation
+    }
+    
+    private let urlInformation: URLInformation
+    private let scheme: String = "http"
+    private let host: String = "www.kobis.or.kr"
+    
+    var url: URL? {
+        var urlComponent = URLComponents()
+        
+        urlComponent.scheme = scheme
+        urlComponent.host = host
+        urlComponent.path = urlInformation.path
+        urlComponent.queryItems = urlInformation.queryItems
+        return urlComponent.url
+    }
+
+    enum URLInformation {
+        case daily(date: String)
+        case month(date: String)
+        
+        var path: String {
+            switch self {
+            case .daily:
+                return ""
+            case .month:
+                return ""
+            }
+        }
+        
+        var queryItems: [URLQueryItem] {
+            var urlQueryItems = [URLQueryItem(name: "key", value: ENV.API_KEY)]
+            
+            switch self {
+            case .daily(let date):
+                urlQueryItems.append(URLQueryItem(name: "fsdf", value: date))
+            case .month(let date):
+                urlQueryItems.append(URLQueryItem(name: "fsdf", value: date))
+            }
+            
+            return urlQueryItems
+        }
+    }
+}
