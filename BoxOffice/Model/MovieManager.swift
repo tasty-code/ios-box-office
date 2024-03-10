0f1d566c253c8d4e1d771e579fda54387ead1765
Matthew(매튜) <krsder@naver.com>
2024-03-10 21:33:52 +0900

    refactor: 영화 이미지 API 기능 구현

--- BoxOffice/Model/MovieManager.swift
+++ BoxOffice/Model/MovieManager.swift
@@ -5,11 +5,12 @@
 //  Created by 강창현 on 2/13/24.
 //
 
-import Foundation
+import UIKit
 
 final class MovieManager {
     var movieDetailData: MovieInfomationDetail?
     var dailyBoxOfficeData: BoxOfficeResult?
+    var movieImageData: Document?
 }
 
 extension MovieManager {
@@ -18,7 +19,7 @@ extension MovieManager {
         completion: @escaping (Result<BoxOfficeResult, NetworkError>) -> Void
     ) {
         let apiService = APIService()
-        let urlString = MovieURL.makeDailyBoxOfficeURL(date: date)
+        let urlString = NetworkURL.makeDailyBoxOfficeURL(date: date)
         
         apiService.fetchData(urlString: urlString) { [weak self] (result: Result<BoxOffice, NetworkError>) in
             switch result {
@@ -37,7 +38,7 @@ extension MovieManager {
         completion: @escaping (Result<MovieInfomationDetail, NetworkError>) -> Void
     ) {
         let apiService = APIService()
-        let urlString = MovieURL.makeMovieInfomationDetailURL(code: code)
+        let urlString = NetworkURL.makeMovieInfomationDetailURL(code: code)
         
         apiService.fetchData(urlString: urlString) { (result: Result<MovieInfomationDetail, NetworkError>) in
             switch result {
@@ -48,5 +49,57 @@ extension MovieManager {
                 completion(.failure(error))
             }
         }
+    }
+    
+    func fetchMoiveImageData(
+        movieName: String,
+        completion: @escaping (Result<Document, NetworkError>) -> Void
+    ) {
+        let apiService = APIService()
+        guard
+            let urlString = NetworkURL.makeMovieImageURL(movieName: movieName)
+        else {
+            print("fetchMoiveImageData\(NetworkError.invalidURLError)")
+            return
+        }
+        
+        apiService.fetchImageData(urlRequest: urlString) { [weak self] (result: Result<MovieImage, NetworkError>) in
+            switch result {
+            case .success(let image):
+                self?.movieImageData = image.documents[0]
+                guard
+                    let data = self?.movieImageData
+                else {
+                    return
+                }
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func makeHttps(data: Document) -> String {
+        let originUrl = data.imageURL
+        var editingUrl = originUrl.components(separatedBy: "://")
+        editingUrl[0] = "https"
+        let resultUrl = "\(editingUrl[0])://\(editingUrl[1])"
+        return resultUrl
+    }
+    
+    func loadImage(data: Document,
+                   completion: @escaping (Result<UIImage, NetworkError>) -> Void) -> UIImage? {
+        let url = makeHttps(data: data)
+        APIService().fetchData(urlString: url) { (result: Result<MovieImage, NetworkError>) in
+            <#code#>
+        }
+//        guard
+//            let url = URL(string: url),
+//            let data = try? Data(contentsOf: url),
+//            let image = UIImage(data: data)
+//        else {
+//            return nil
+//        }
+//        return image
     }
 }
