cac53ecfad4bed9d4ba785a2aedddfa4f63b2cf9
Matthew(매튜) <krsder@naver.com>
2024-03-10 21:34:48 +0900

    fix: Json 디코딩 타입 수정

--- BoxOffice/Model/Movie/Image/Document.swift
+++ BoxOffice/Model/Movie/Image/Document.swift
@@ -9,19 +9,20 @@ struct Document: Codable {
     let collection: String
     let thumbnailURL: String
     let imageURL: String
-    let width, height: Int
-    let displaySitename: String
+    let width: Int
+    let height: Int
+    let displaySiteName: String
     let docURL: String
     let datetime: String
 
     enum CodingKeys: String, CodingKey {
         case collection
-        case thumbnailURL
-        case imageURL
+        case thumbnailURL = "thumbnail_url"
+        case imageURL = "image_url"
         case width
         case height
-        case displaySitename = "displaySiteName"
-        case docURL
+        case displaySiteName = "display_sitename"
+        case docURL = "doc_url"
         case datetime
     }
 }
--- BoxOffice/Model/Movie/Image/Meta.swift
+++ BoxOffice/Model/Movie/Image/Meta.swift
@@ -9,4 +9,10 @@ struct Meta: Codable {
     let totalCount: Int
     let pageableCount: Int
     let isEnd: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case isEnd = "is_end"
+        case pageableCount = "pageable_count"
+        case totalCount = "total_count"
+    }
 }
