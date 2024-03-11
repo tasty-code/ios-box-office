f14d584a54d6bd83d26968741784ecf40ebc947f
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-03-11 10:54:14 +0900

    feat: kakaoAPIKEY 추가

--- BoxOffice/Infra/Util/Extension/Bundle+.swift
+++ BoxOffice/Infra/Util/Extension/Bundle+.swift
@@ -11,4 +11,15 @@ extension Bundle {
         }
         return key
     }
+    
+    var kakaoApiKey: String {
+        guard let file = self.path(forResource: "Info", ofType: "plist"),
+              let resoure = NSDictionary(contentsOfFile: file) else {
+            fatalError("Info.plist 파일을 찾을 수 없습니다.")
+        }
+        guard let key = resoure["Kakao_API_KEY"] as? String else {
+            fatalError("Kakao_API_KEY 키를 찾을 수 없습니다.")
+        }
+        return key
+    }
 }
