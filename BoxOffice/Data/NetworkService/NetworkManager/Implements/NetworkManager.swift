573f5033fdeb493a39c13b978ef3e841d58fb244
suojae3 <suojae3@gmail.com>
2024-03-11 21:49:40 +0900

    chore: 변수명 변경

--- BoxOffice/Data/NetworkService/NetworkManager/Implements/NetworkManager.swift
+++ BoxOffice/Data/NetworkService/NetworkManager/Implements/NetworkManager.swift
@@ -3,17 +3,17 @@ import Foundation
 
 
 final class NetworkManager: NetworkManagerProtocol {
-    private let sessionProvider: SessionProvidable
+    private let session: SessionProvidable
     private let decoder: JsonDecodeProtocol
     
     init(sessionProvider: SessionProvidable, decoder: JsonDecodeProtocol) {
-        self.sessionProvider = sessionProvider
+        self.session = sessionProvider
         self.decoder = decoder
     }
     
     func bringNetworkResult<T: Decodable>(from request: URLRequest) async -> Result<T, NetworkError> {
         
-        let result = await sessionProvider.requestAPI(using: request)
+        let result = await session.requestAPI(using: request)
         
         switch result {
         case .success(let networkResponse):
