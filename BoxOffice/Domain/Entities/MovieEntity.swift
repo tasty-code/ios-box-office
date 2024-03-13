9ac6ae60b55bd8ab1a6914d3592bd96a0d169c1d
eunjuchoi <eunjuzzoo@naver.com>
2024-03-13 16:44:57 +0900

    add: MovieEntity 추가

---
+++ BoxOffice/Domain/Entities/MovieEntity.swift
@@ -0,0 +1,19 @@
+//
+//  MovieEntity.swift
+//  BoxOffice
+//
+//  Created by EUNJU on 3/13/24.
+//
+
+import Foundation
+
+struct MovieEntity {
+    let director: String
+    let productYear: String
+    let openDate: String
+    let showTime: String
+    let watchGrade: String
+    let nation: String
+    let genres: String
+    let actors: String
+}
