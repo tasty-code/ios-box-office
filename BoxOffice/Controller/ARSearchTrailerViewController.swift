0b5f02978ad6f6506f1ae0e5a961c4f5023f9174
eensungkim <eensungkim@gmail.com>
2024-03-13 17:20:42 +0900

    feat: ARSCNView 연결

--- BoxOffice/Controller/ARSearchTrailerViewController.swift
+++ BoxOffice/Controller/ARSearchTrailerViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 import ARKit
 
 class ARSearchTrailerViewController: UIViewController, ARSessionDelegate {
-    
     let session: ARSession = ARSession()
+    
+    let trailerSearchView: ARSCNView = ARSCNView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadAR()
     }
     
+    override func loadView() {
+        view = trailerSearchView
+        trailerSearchView.session = session
+    }
+    
     func loadAR() {
         let configuration = ARImageTrackingConfiguration()
+//        configuration.trackingImages
         session.delegate = self
         session.run(configuration)
     }
