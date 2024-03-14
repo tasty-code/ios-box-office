ec21ad5b3a3484a5c71b37e5d56dc73c15ebc114
eensungkim <eensungkim@gmail.com>
2024-03-14 13:48:15 +0900

    refactor: window.backgroundColor 대신 기본 appearance 설정으로 변경

--- BoxOffice/Controller/AppDelegate.swift
+++ BoxOffice/Controller/AppDelegate.swift
@@ -11,6 +11,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        
         return true
     }
 
