186da18c7412c310db6841d25d03b78d969804c9
Changhyun Kang <changhyun.kyle@gmail.com>
2024-03-15 17:40:34 +0900

    refactor: URLSession 패치 로직 변경에 따른 프로토콜 수정

--- BoxOffice/Network/Protocol/URLSessionProtocol.swift
+++ BoxOffice/Network/Protocol/URLSessionProtocol.swift
@@ -8,13 +8,7 @@
 import Foundation
 
 protocol URLSessionProtocol {
-    func dataTask(
-        with url: URL,
-        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-    ) -> URLSessionDataTaskProtocol
-    
-    func dataTask(
-        with request: URLRequest,
-        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-    ) -> URLSessionDataTaskProtocol
+    func requestData(
+        with request: URLRequest
+    ) async throws -> (Data, URLResponse)
 }
