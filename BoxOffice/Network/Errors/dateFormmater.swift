806962dd0c12ca57793a3ee88fda5bfa88901770
yoruck2 <yoruck2@gmail.com>
2024-03-19 19:27:31 +0900

    feat: dateFormmater 추가

--- BoxOffice/Network/Errors/dateFormmater.swift
+++ BoxOffice/Network/Errors/dateFormmater.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Formatter {
+    func formatting() -> String? {
+        let dateFormatter = DateFormatter()
+        
+        let today = Date()
+        let timezone = TimeZone.autoupdatingCurrent
+        let secondsFromGMT = timezone.secondsFromGMT(for: today)
+        let localizedDate = today.addingTimeInterval(TimeInterval(secondsFromGMT))
+
+        let yesterday = Calendar(identifier: .gregorian).date(byAdding: .day, value: -2, to: localizedDate)
+
+        let dateString = dateFormatter.string(for: yesterday)
+        
+        return dateString
+    }
+}
