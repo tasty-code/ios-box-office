4fde8761a733d4d808d2ed7ff2c192f22af563ad
yoruck2 <yoruck2@gmail.com>
2024-03-19 19:33:56 +0900

    feat: MovieListViewController하위 뷰 추가
    collectionVIew, activityIndicator, refreshContr

--- BoxOffice/MovieListViewController.swift
+++ BoxOffice/MovieListViewController.swift
@@ -4,61 +4,131 @@
 //
 //  Created by kjs on 13/01/23.
 //
-
+import SwiftUI
 import UIKit
-import OSLog
+
 final class MovieListViewController: UIViewController {
     
-    private let networking = NetworkManager.shared
-    private let networkAPI = NetworkAPI()
+    private let networkManager = NetworkManager.shared
+    private var boxOfficeDataArray:[DailyBoxOfficeDTO.BoxOfficeDTO.MovieInfo] = []
+    let apiBuilder = APIURLBuilder()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let CV = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return CV
+    }()
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = false
+        activityIndicator.style = .large
+        activityIndicator.startAnimating()
+        return activityIndicator
+    }()
+    
+    private lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(reload), for: .valueChanged)
+        
+        return refreshControl
+        
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        callDailyBoxOfficeAPI()
-        callMovieDetailAPI()
-
-        os_log("네트워크 오류남", log: .network, type: .error)
-        os_log("네트워크 오류남", type: .error)
-        os_log("default")
-    }
-    
-    private func callMovieDetailAPI() {
-        let url = networkAPI.buildMovieDetailAPI(movieCode: .movieCode)
-        networking.performRequest(with: url) { result in
-            switch result {
-            case .success(let data):
-                guard let data = data,
-                      let decodedData = JSONParser().decode(data as! Data, DTO: DailyBoxOfficeResultDTO.self)
-                else {
-                    return
-                }
-                print(decodedData)
-            case .failure(let error):
-                print(error)
+        title = Date().yesterday
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.refreshControl = refreshControl
+        setupCollectionView()
+        view.addSubview(activityIndicator)
+        
+        Task {
+            let data = try await networkManager.performRequest(with: apiBuilder.buildDailyBoxOfficeAPI(targetDate: APIURLCompnents.QueryValues.targetDate ?? ""))
+                
+            guard let DTO = JSONParser().decode(data, DTO: DailyBoxOfficeDTO.self),
+                  let boxOfficeList = DTO.boxOfficeResult.dailyBoxOfficeList
+            else {
+                return
             }
+            
+            boxOfficeDataArray = boxOfficeList
+            collectionView.reloadData()
+            activityIndicator.isHidden = true
         }
     }
     
-    private func callDailyBoxOfficeAPI() {
-        let url = networkAPI.buildDailyBoxOfficeAPI(targetDate: .targetDate, keys: .itemPerPage, values: "2")
-        networking.performRequest(with: url) { result in
-            switch result {
-            case .success(let data):
-                guard let data = data,
-                      let decodedData = JSONParser().decode(data as! Data, DTO: DailyBoxOfficeResultDTO.self)
-                else {
-                    return
-                }
-                print(decodedData,"\n -------------------------")
-            case .failure(let error):
-                print(error)
-            }
+    @objc
+    func reload() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            self.collectionView.reloadData()
+            self.refreshControl.endRefreshing()
         }
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(MovieListCollectionViewCell.self, forCellWithReuseIdentifier: "MovieListCollectionViewCell")
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 }
 
-extension OSLog {
-    private static var subsystem = Bundle.main.bundleIdentifier!
+extension MovieListViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieListCollectionViewCell", for: indexPath) as? MovieListCollectionViewCell else {
+            fatalError("fatalError")
+        }
+        cell.configureCell(with: boxOfficeDataArray[indexPath.row])
+        cell.addSubview(MovieListCollectionViewCell())
+        return cell
+    }
     
-    static let network = OSLog(subsystem: subsystem, category: "Network")
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        boxOfficeDataArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.frame.width
+        return CGSize(width: width, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
 }
+
+
+
+//import SwiftUI
+//struct ViewControllerRepresentable: UIViewControllerRepresentable {
+//    
+//    // update
+//    func updateUIViewController(_ uiViewController: UIViewController, context: Context){
+//        
+//    }
+//    // makeui
+//    @available(iOS 13.0, *)
+//    func makeUIViewController(context: Context) -> UIViewController {
+//        MovieListViewController()
+//    }
+//}
+//
+//struct ViewController_Previews: PreviewProvider {
+//    static var previews: some View{
+//        ViewControllerRepresentable().previewDisplayName("아이폰 15프로")
+//    }
+//}
+
