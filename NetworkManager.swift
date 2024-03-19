c4f26d824395ef92b143d8eb5631f821a3e0d814
yoruck2 <yoruck2@gmail.com>
2024-03-19 19:30:32 +0900

    refactor: performRequest async 메서드로 수정
    completoinHandler 에서 swift cocurrency로

--- NetworkManager.swift
+++ NetworkManager.swift
@@ -8,53 +8,50 @@
 import Foundation
 
 class NetworkManager {
-    
+
     static let shared = NetworkManager()
-    private let networkAPI = NetworkAPI()
     
     private init() {}
     
-    typealias NetworkCompletion<T: Decodable> = (Result<T, NetworkError>) -> Void
-    
-//    func creatURLRequest(url: URL, ) throws -> URLRequest {
-//        let url = try asURL(baseURL)
-//        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
-//        urlRequest.httpMethod = method.rawValue
-//        urlRequest = self.makeHeader(for: urlRequest)
-//        
-//        return try makeParameter(for: urlRequest, with: url)
-//    }
-    
-    func performRequest(with url: URL?, completion: @escaping (Result<Decodable?, NetworkError>) -> Void) {
+    func performRequest(with url: URL?) async throws -> Data {
         guard let url else {
-            return
+            throw NetworkError.dataError
         }
         let session = URLSession(configuration: .default)
-        var request = URLRequest(url: url)
-//        request.httpMethod = HTTPMethod
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("application/json", forHTTPHeaderField: "Accept")
-//        request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+        let request = makeRequest(with: url, method: .get, headers: nil)
+        
+        let (data, response) = try await session.data(for: request)
 
-        let task = session.dataTask(with: url) { data, response, error in
-            print(url)
-            guard error == nil else {
-                print("네트워크 에러")
-                completion(.failure(.netWorkingError))
-                return
-            }
-            guard let safeData = data else {
-                completion(.failure(.dataError))
-                return
-            }
-            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
-                completion(.failure(.netWorkingError))
-                return
-            }
-            completion(.success(safeData))
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+            throw NetworkError.responseError
         }
-        task.resume()
+        guard (200 ..< 299) ~= statusCode else {
+            throw responseVerify(with: response as! HTTPURLResponse)
+        }
+        return data
+    }
+    
+    func makeRequest(with url: URL, method: HTTPMethod, headers: [String: String]?) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        headers?.forEach { key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        return request
+    }
+    
+    private func responseVerify(with HTTPResponse: HTTPURLResponse) -> Error {
+        switch HTTPResponse.statusCode {
+        case (300...399):
+            return HTTPError.redirectionMessages(HTTPResponse.statusCode, HTTPResponse.debugDescription)
+        case (400...499):
+            return HTTPError.clientErrorResponses(HTTPResponse.statusCode, HTTPResponse.debugDescription)
+        case (500...599):
+            return HTTPError.serverErrorResponses(HTTPResponse.statusCode, HTTPResponse.debugDescription)
+        default:
+            return HTTPError.networkFailError(HTTPResponse.statusCode)
+        }
     }
 }
-
-
+                              
