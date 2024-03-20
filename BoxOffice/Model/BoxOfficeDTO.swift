67147c9dd46c852cfcddc8bff6bfd1da0888d4ca
wlsdud-dev <hjymail1025@gmail.com>
2024-03-20 11:22:54 +0900

    refactor: 박스오피스 개체 수정

--- BoxOffice/Model/BoxOfficeDTO.swift
+++ BoxOffice/Model/BoxOfficeDTO.swift
@@ -5,57 +5,58 @@
 //  Created by Jin-Mac on 2/14/24.
 //
 
-struct BoxOfficeData: Codable {
+struct BoxOfficeDTO: Decodable {
     let boxOfficeResult: BoxOfficeResult
 }
 
-struct BoxOfficeResult: Codable {
+struct BoxOfficeResult: Decodable {
     let boxofficeType: String
     let showRange: String
     let dailyBoxOfficeList: [DailyBoxOfficeList]
 }
 
-struct DailyBoxOfficeList: Codable {
+struct DailyBoxOfficeList: Decodable {
     let number: String
     let rank: String
     let rankIncrement: String
     let rankOldAndNew: RankOldAndNew
-    let movieCd: String
+    let movieCode: String
     let movieName: String
     let openDate: String
-    let salesAmount: String
-    let salesShare: String
+    let salesDailyAmount: String
+    let salesDailyShare: String
     let salesIncrement: String
-    let salesChange: String
-    let salesAccount: String
-    let audienceCount: String
+    let salesChangeRatio: String
+    let salesTotalAmount: String
+    let audienceDailyCount: String
     let audienceIncrement: String
-    let audienceChange: String
-    let audienceAccount: String
+    let audienceChangeRatio: String
+    let audienceTotalAmount: String
     let screenCount: String
     let showCount: String
     
     enum CodingKeys: String, CodingKey{
         case number = "rnum"
         case rankIncrement = "rankInten"
+        case movieCode = "movieCd"
         case movieName = "movieNm"
         case openDate = "openDt"
-        case salesAmount = "salesAmt"
-        case salesShare = "salesShare"
+        case salesDailyAmount = "salesAmt"
+        case salesDailyShare = "salesShare"
         case salesIncrement = "salesInten"
-        case salesChange = "salesChange"
-        case salesAccount = "salesAcc"
-        case audienceCount = "audiCnt"
+        case salesChangeRatio = "salesChange"
+        case salesTotalAmount = "salesAcc"
+        case audienceDailyCount = "audiCnt"
         case audienceIncrement = "audiInten"
-        case audienceChange = "audiChange"
-        case audienceAccount = "audiAcc"
+        case audienceChangeRatio = "audiChange"
+        case audienceTotalAmount = "audiAcc"
         case screenCount = "scrnCnt"
         case showCount = "showCnt"
-        case rankOldAndNew, rank, movieCd
+        case rankOldAndNew, rank
     }
 }
 
-enum RankOldAndNew: String, Codable {
+enum RankOldAndNew: String, Decodable {
     case new = "NEW"
     case old = "OLD"
 }
