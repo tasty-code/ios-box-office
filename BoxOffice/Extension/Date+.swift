51c62470472e82161cd91ac69dfe51700f61664d
yoruck2 <yoruck2@gmail.com>
2024-03-20 11:49:54 +0900

    refactor: yesterday 메서드로 통합

--- BoxOffice/Extension/Date+.swift
+++ BoxOffice/Extension/Date+.swift
@@ -8,28 +8,20 @@
 import Foundation
 
 extension Date {
-    var yesterday: String {
+    static let yyyyMMdd = "yyyyMMdd"
+    static let yyyyMMddHyphen = "yyyy-MM-dd"
+    
+    func yesterday(format: String) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMdd"
+        dateFormatter.dateFormat = format
         let today = Date()
         let timezone = TimeZone.autoupdatingCurrent
         let secondsFromGMT = timezone.secondsFromGMT(for: today)
         let localizedDate = today.addingTimeInterval(TimeInterval(secondsFromGMT))
-        let yesterday = Calendar(identifier: .gregorian).date(byAdding: .day, value: -2, to: localizedDate)
+        let yesterday = Calendar(identifier: .gregorian).date(byAdding: .day, value: -1, to: localizedDate)
         guard let dateString = dateFormatter.string(for: yesterday) else {
             return "알 수 없는 날짜"
         }
         return dateString
-    }
-    
-    func addHyphen(dateString: String) -> String? {
-        let inputFormatter = DateFormatter()
-        inputFormatter.dateFormat = "yyyyMMdd"
-        if let date = inputFormatter.date(from: dateString) {
-            let outputFormatter = DateFormatter()
-            outputFormatter.dateFormat = "yyyy-MM-dd"
-            return outputFormatter.string(from: date)
-        }
-        return nil
     }
 }
