a622e0efeb09ce33cd14d89d912f07523dfee4aa
wlsdud-dev <hjymail1025@gmail.com>
2024-03-20 16:43:49 +0900

    feat: string 익스텐션 숫자 세자리 , 추가 기능 구현

---
+++ BoxOffice/String+.swift
@@ -0,0 +1,22 @@
+//
+//  String+.swift
+//  BoxOffice
+//
+//  Created by Jin-Mac on 3/20/24.
+//
+
+import Foundation
+
+extension String {
+    
+    /// 숫자가 세 자리 이상 넘어가면 ,를 활용하는 메서드
+    func formatNumberString() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.locale = Locale.current
+        if let number = Double(self) {
+            return formatter.string(from: NSNumber(value: number)) ?? self
+        }
+        return self
+    }
+}
